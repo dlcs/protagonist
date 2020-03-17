3595bd28ad7f4c8cc1718a895a559bf4413e4540
Donald Gray <donald.gray@digirati.com>
2020-03-17 12:15:42 +0000

    Update ThumbReorganiser to remove 'old' format of bounded thumbs

--- DLCS.Repository/Assets/ThumbReorganiser.cs
+++ DLCS.Repository/Assets/ThumbReorganiser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using DLCS.Core.Collections;
 using DLCS.Core.Threading;
@@ -21,6 +22,7 @@ namespace DLCS.Repository.Assets
         private readonly IAssetRepository assetRepository;
         private readonly IThumbnailPolicyRepository thumbnailPolicyRepository;
         private readonly AsyncKeyedLock asyncLocker = new AsyncKeyedLock();
+        private static Regex BoundedThumbRegex = new Regex("^[0-9]+.jpg$");
 
         public ThumbReorganiser(
             IBucketReader bucketReader,
@@ -39,7 +41,8 @@ namespace DLCS.Repository.Assets
             // Create lock on rootKey unique value (bucket + target key)
             using var processLock = await asyncLocker.LockAsync(rootKey.ToString());
             
-            if (await HasCurrentLayout(rootKey))
+            var keysInTargetBucket = await bucketReader.GetMatchingKeys(rootKey);
+            if (HasCurrentLayout(rootKey, keysInTargetBucket))
             {
                 logger.LogDebug("{RootKey} has expected current layout", rootKey);
                 return;
@@ -76,15 +79,15 @@ namespace DLCS.Repository.Assets
             // All the thumbnail jpgs will already exist and need copied up to root
             await CreateThumbnails(rootKey, boundingSquares, thumbnailSizes);
 
-            // Create sizes.json last, as this dictates whether this process will be attempted again
+            // Create sizes json file last, as this dictates whether this process will be attempted again
             await CreateSizesJson(rootKey, thumbnailSizes);
+
+            // Clean up legacy format from before /open /auth paths
+            await CleanupRootConfinedSquareThumbs(rootKey, keysInTargetBucket);
         }
 
-        private async Task<bool> HasCurrentLayout(ObjectInBucket rootKey)
-        {
-            var keys = await bucketReader.GetMatchingKeys(rootKey);
-            return keys.Contains($"{rootKey.Key}{thumbConsts.SizesJsonKey}");
-        }
+        private static bool HasCurrentLayout(ObjectInBucket rootKey, string[] keysInTargetBucket) =>
+            keysInTargetBucket.Contains($"{rootKey.Key}{thumbConsts.SizesJsonKey}");
 
         private static Size GetMaxAvailableThumb(Asset asset, ThumbnailPolicy policy)
         {
@@ -130,6 +133,37 @@ namespace DLCS.Repository.Assets
             await bucketReader.WriteToBucket(sizesDest, JsonConvert.SerializeObject(thumbnailSizes), "application/json");
         }
 
+        private async Task CleanupRootConfinedSquareThumbs(ObjectInBucket rootKey, string[] s3ObjectKeys)
+        {
+            // This is an interim method to clean up the first implementation of /thumbs/ handling
+            // which created all thumbs at root and sizes.json, rather than s.json
+            // We output s.json now. Previously this was sizes.json
+            const string oldSizesJsonKey = "sizes.json";
+            
+            if (s3ObjectKeys.IsNullOrEmpty()) return;
+            
+            List<ObjectInBucket> toDelete = new List<ObjectInBucket>(s3ObjectKeys.Length);
+            
+            foreach (var key in s3ObjectKeys)
+            {
+                string item = key.Replace(rootKey.Key, string.Empty);
+                if (BoundedThumbRegex.IsMatch(item) || item == oldSizesJsonKey)
+                {
+                    logger.LogDebug($"Deleting legacy confined-thumb object: '{key}'");
+                    toDelete.Add(new ObjectInBucket
+                    {
+                        Bucket = rootKey.Bucket,
+                        Key = key
+                    });
+                }
+            }
+
+            if (toDelete.Count > 0)
+            {
+                await bucketReader.DeleteFromBucket(toDelete.ToArray());
+            }
+        }
+        
         public void DeleteOldLayout()
         {
             throw new NotImplementedException("Not yet! Need to be sure of all the others first!");
