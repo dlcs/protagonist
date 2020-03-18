4c95c47db4f3132e0cebc59babdb6dde3b6881b7
Donald Gray <donald.gray@digirati.com>
2020-03-18 16:41:10 +0000

    Configure logging for thumbs

--- Thumbs/Program.cs
+++ Thumbs/Program.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Thumbs
 {
@@ -14,13 +15,18 @@ namespace Thumbs
         
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders()
+                        .AddConsole();
+                })
                 .ConfigureAppConfiguration((context, builder) =>
                 {
                     builder.AddSystemsManager(configurationSource =>
                     {
                         configurationSource.Path = "/thumbs/";
                         configurationSource.ReloadAfter = TimeSpan.FromMinutes(90);
-
+                    
                         // Using ParameterStore optional if Development
                         configurationSource.Optional = context.HostingEnvironment.IsDevelopment();
                     });
