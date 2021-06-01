bff49cd75d1b6f2e40ae0813d70397ab1c2d0101
Donald Gray <donald.gray@digirati.com>
2021-06-01 13:16:21 +0100

    Add apiKeys request to client

---
+++ Portal/Pages/Keys/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portal.Features.Keys;
+
+namespace Portal.Pages.Keys
+{
+    public class Index : PageModel
+    {
+        private readonly IMediator mediator;
+        
+        [BindProperty]
+        public IEnumerable<string> ApiKeys { get; set; } = Enumerable.Empty<string>();
+
+        public Index(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        
+        public async Task OnGetAsync()
+        {
+            ApiKeys = await mediator.Send(new GetCustomerApiKeys()) ?? Enumerable.Empty<string>();
+        }
+    }
+}
