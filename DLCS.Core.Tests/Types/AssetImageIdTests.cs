680bb88eaac3c8286cbc60999d22fbd9a002a62e
Donald Gray <donald.gray@digirati.com>
2021-07-20 16:05:27 +0100

    Add AssetImageId record type

---
+++ DLCS.Core.Tests/Types/AssetImageIdTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DLCS.Core.Types;
+using FluentAssertions;
+using Xunit;
+
+namespace DLCS.Core.Tests.Types
+{
+    public class AssetImageIdTests
+    {
+        [Fact]
+        public void ToString_CorrectFormat()
+        {
+            var assetImageId = new AssetImageId(19, 4, "my-first-image");
+            const string expected = "19/4/my-first-image";
+
+            assetImageId.ToString().Should().Be(expected);
+        }
+        
+        [Fact]
+        public void FromString_ReturnsExpected()
+        {
+            const string assetId = "19/4/my-first-image";
+
+            var assetImageId = AssetImageId.FromString(assetId);
+
+            assetImageId.Customer.Should().Be(19);
+            assetImageId.Space.Should().Be(4);
+            assetImageId.Image.Should().Be("my-first-image");
+        }
+
+        [Theory]
+        [InlineData("image")]
+        [InlineData("1/2/image/easrwt")]
+        [InlineData("customer/2/image")]
+        [InlineData("1/space/image")]
+        public void FromString_Throws_IfInvalidFormat(string assetId)
+        {
+            Action action = () => AssetImageId.FromString(assetId);
+            action.Should().Throw<FormatException>();
+        }
+    }
+}
---
+++ DLCS.Core/Types/AssetImageId.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace DLCS.Core.Types
+{
+    /// <summary>
+    /// A record that represents an identifier for a DLCS Asset.
+    /// </summary>
+    /// <param name="Customer">Id of customer</param>
+    /// <param name="Space">Id of space</param>
+    /// <param name="Image">Id of image</param>
+    public record AssetImageId(int Customer, int Space, string Image)
+    {
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new();
+            PrintMembers(stringBuilder);
+            return stringBuilder.ToString();
+        }
+
+        protected virtual bool PrintMembers(StringBuilder builder)
+        {
+            builder.Append(Customer);
+            builder.AppendFormat("/{0}/{1}", Space, Image);
+            return true;
+        }
+
+        /// <summary>
+        /// Create a new AssetImageId from string in format customer/space/image
+        /// </summary>
+        /// <param name="assetImageId">string representing assetImageId</param>
+        /// <returns>New <see cref="AssetImageId"/> record</returns>
+        /// <exception cref="FormatException">Thrown if string not in expected format</exception>
+        public static AssetImageId FromString(string assetImageId)
+        {
+            var parts = assetImageId.Split("/", StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 3)
+            {
+                throw new FormatException("AssetImageId string must be in format customer/space/image");
+            }
+
+            return new AssetImageId(int.Parse(parts[0]), int.Parse(parts[1]), parts[2]);
+        }
+    }
+}
