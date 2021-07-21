10f7e1caad0275e1a20611e75587911eb18e986a
Donald Gray <donald.gray@digirati.com>
2021-07-21 14:29:10 +0100

    Make prefix optional for ImageRequest.Parse

---
+++ IIIF.Tests/ImageApi/ImageRequestTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using IIIF.ImageApi;
+using Xunit;
+
+namespace IIIF.Tests.ImageApi
+{
+    public class ImageRequestTests
+    {
+        [Theory]
+        [InlineData("/image", "")]
+        [InlineData("image", "")]
+        [InlineData("/path-prefix/image/", "path-prefix/")]
+        [InlineData("path-prefix/image/", "path-prefix/")]
+        public void Parse_Correct_BasePath(string path, string prefix)
+        {
+            // Act
+            var result = ImageRequest.Parse(path, prefix);
+            
+            // Assert
+            result.IsBase.Should().BeTrue();
+        }
+        
+        [Theory]
+        [InlineData("/image/info.json", "")]
+        [InlineData("image/info.json", "")]
+        [InlineData("/path-prefix/image/info.json", "path-prefix/")]
+        [InlineData("path-prefix/image/info.json", "path-prefix/")]
+        public void Parse_Correct_InfoJson(string path, string prefix)
+        {
+            // Act
+            var result = ImageRequest.Parse(path, prefix);
+            
+            // Assert
+            result.IsInformationRequest.Should().BeTrue();
+        }
+    }
+}
