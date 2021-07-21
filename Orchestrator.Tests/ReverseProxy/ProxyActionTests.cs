d7d5c477252ede15524d8256f1e9591f0385dc5d
Donald Gray <donald.gray@digirati.com>
2021-07-21 17:01:27 +0100

    Add basic ProxyAction test

---
+++ Orchestrator.Tests/ReverseProxy/ProxyActionTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Orchestrator.ReverseProxy;
+using Xunit;
+
+namespace Orchestrator.Tests.ReverseProxy
+{
+    public class ProxyActionTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void HasPath_False_IfNullOrWhiteSpace(string path)
+        {
+            // Act
+            var proxyAction = new ProxyAction(ProxyTo.Thumbs, path);
+            
+            // Assert
+            proxyAction.HasPath.Should().BeFalse();
+        }
+    }
+}
--- Orchestrator/ReverseProxy/ProxyAction.cs
+++ Orchestrator/ReverseProxy/ProxyAction.cs
@@ -7,15 +7,40 @@
     /// <param name="Path">Path to proxy to, if rewritten</param>
     public record ProxyAction(ProxyTo Target, string? Path = null)
     {
+        /// <summary>
+        /// Get value indicating whether action has a path
+        /// </summary>
         public bool HasPath => !string.IsNullOrWhiteSpace(Path);
     }
     
+    /// <summary>
+    /// Enum representing potential locations to proxy to
+    /// </summary>
     public enum ProxyTo
     {
+        /// <summary>
+        /// Unknown, fallback value
+        /// </summary>
         Unknown,
+        
+        /// <summary>
+        /// Orchestrator for handling standard requests (legacy, will be removed)
+        /// </summary>
         Orchestrator,
+        
+        /// <summary>
+        /// Thumbs services, for handling requests for thumbs
+        /// </summary>
         Thumbs,
+        
+        /// <summary>
+        /// Image-server/image-server cluster
+        /// </summary>
         ImageServer,
+        
+        /// <summary>
+        /// Caching reverse proxy (Varnish) 
+        /// </summary>
         CachingProxy
     }
 }
