b2540f6cbf107feda9277ca95777965fd8329fa1
Donald Gray <donald.gray@digirati.com>
2021-08-20 09:49:14 +0100

    Add TemplatedFolders class

---
+++ DLCS.Model.Tests/Templates/TemplatedFoldersTests.cs
@@ -0,0 +1,61 @@
+﻿using DLCS.Core.Types;
+using DLCS.Model.Templates;
+using FluentAssertions;
+using Xunit;
+
+namespace DLCS.Model.Tests.Templates
+{
+    public class TemplatedFoldersTests
+    {
+        [Fact]
+        public void GenerateTemplate_ReturnsExpected_ImageNameUnaltered_IfImageName8CharsOrLess()
+        {
+            // Arrange
+            char s = System.IO.Path.DirectorySeparatorChar;
+            var root = "folder";
+            var asset = new AssetId(10, 20, "foobarba");
+            var template = $"{s}{{root}}{s}{{customer}}{s}{{space}}{s}{{image}}";
+            var expected = $"{s}folder{s}10{s}20{s}foobarba";
+
+            // Act
+            var result = TemplatedFolders.GenerateTemplate(template, root, asset);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void GenerateTemplate_ReturnsExpected_ImageNameAltered_IfImageName8CharsOrLess()
+        {
+            // Arrange
+            char s = System.IO.Path.DirectorySeparatorChar;
+            var root = "folder";
+            var asset = new AssetId(10, 20, "foobarbazqux");
+            var template = $"{s}{{root}}{s}{{customer}}{s}{{space}}{s}{{image}}";
+            var expected = $"{s}folder{s}10{s}20{s}fo{s}ob{s}ar{s}ba{s}foobarbazqux";
+
+            // Act
+            var result = TemplatedFolders.GenerateTemplate(template, root, asset);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void GenerateTemplate_ReturnsExpected_ImageNameNotReplaced_IfReplaceImageNameFalse()
+        {
+            // Arrange
+            char s = System.IO.Path.DirectorySeparatorChar;
+            var root = "folder";
+            var asset = new AssetId(10, 20, "foobarbazqux");
+            var template = $"{s}{{root}}{s}{{customer}}{s}{{space}}{s}{{image}}";
+            var expected = $"{s}folder{s}10{s}20{s}{{image}}";
+
+            // Act
+            var result = TemplatedFolders.GenerateTemplate(template, root, asset, false);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ DLCS.Model/Templates/TemplatedFolders.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using DLCS.Core.Types;
+
+namespace DLCS.Model.Templates
+{
+    /// <summary>
+    /// Collection of methods for dealing with folder templates for saving assets to local disk.
+    /// </summary>
+    /// <remarks>This logic has been copied over from Deliverator implementation.</remarks> 
+    public static class TemplatedFolders
+    {
+        private const string Root = "{root}";
+        private const string Customer = "{customer}";
+        private const string Space = "{space}";
+        private const string Image = "{image}";
+
+        /// <summary>
+        /// Generate a folder template using provided details.
+        /// </summary>
+        /// <param name="template">The basic template, e.g. {root}\{customer}\{space}\{image}</param>
+        /// <param name="root">The root of the template, used as {root} param.</param>
+        /// <param name="asset">Used to populate {customer}, {space} and, optionally, {image} properties.</param>
+        /// <param name="replaceImage">If true {image} is replaced, else it is left untouched</param>
+        /// <returns>New string with replacements made.</returns>
+        public static string GenerateTemplate(string template, string root, AssetId asset, bool replaceImage = true)
+        {
+            var replacements = template
+                .Replace(Root, root)
+                .Replace(Customer, asset.Customer.ToString())
+                .Replace(Space, asset.Space.ToString());
+
+            return replaceImage
+                ? replacements.Replace(Image, SplitImageNameToFolders(asset.Asset, Path.DirectorySeparatorChar))
+                : replacements;
+        }
+
+        private static string SplitImageNameToFolders(string name, char separator)
+            => name.Length <= 8
+                ? name
+                : string.Concat(
+                    name[..2], separator,
+                    name[2..4], separator,
+                    name[4..6], separator,
+                    name[6..8], separator,
+                    name);
+    }
+}
