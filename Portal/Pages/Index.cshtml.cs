fdf3501dc112e030d4e87dc71f6ce7b767949008
tomcrane <tom@nanoplanet.com>
2021-09-29 10:35:01 +0100

    Add a getting started banner for new portal users

--- Portal/Pages/Index.cshtml.cs
+++ Portal/Pages/Index.cshtml.cs
@@ -2,23 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Portal.Features.Spaces.Requests;
+using Index = Portal.Pages.Spaces.Index;
 
 namespace Portal.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IMediator mediator;
         private readonly ILogger<IndexModel> _logger;
+        
+        [BindProperty]
+        public List<Index.SpaceModel> Spaces { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IMediator mediator, ILogger<IndexModel> logger)
         {
+            this.mediator = mediator;
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                var spaces = await mediator.Send(new GetAllSpaces());
+                Spaces = spaces.Select(s => new Index.SpaceModel
+                {
+                    SpaceId = s.Id,
+                    Name = s.Name,
+                    Created = s.Created
+                }).ToList();
+            }
+            else
+            {
+                Spaces = new List<Index.SpaceModel>(0);
+            }
         }
     }
 }
