a559c1f22716a73e6e37b57981ea1d8795e4a461
Donald Gray <donald.gray@digirati.com>
2021-10-22 08:05:19 +0100

    Upgrade yarp to rc-1

--- Orchestrator/Features/Images/ImageRouteHandlers.cs
+++ Orchestrator/Features/Images/ImageRouteHandlers.cs
@@ -39,7 +39,7 @@ namespace Orchestrator.Features.Images
             DefaultTransformer = HttpTransformer.Default;
             
             // TODO - make this configurable, potentially by target
-            RequestOptions = new ForwarderRequestConfig {Timeout = TimeSpan.FromSeconds(100)};  
+            RequestOptions = new ForwarderRequestConfig {ActivityTimeout = TimeSpan.FromSeconds(60)};  
         }
 
         /// <summary>
