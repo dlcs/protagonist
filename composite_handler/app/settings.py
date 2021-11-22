bf7dbc1eb1e0a45efbcca196f3ff9142fb3e6e3c
Daniel Grant <daniel.grant@digirati.com>
2021-11-22 13:58:49 +0000

    Allowing all hosts

--- composite_handler/app/settings.py
+++ composite_handler/app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-p8%3wt_7j(fur$*9m)pi+=9%18_x)zhjl)5)4vk)gcoc=efx-u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
