3d11adcb0592d44b706aea02ea9c80a850866fec
Daniel Grant <daniel.grant@digirati.com>
2021-11-23 13:58:19 +0000

    Linting

--- composite_handler/app/api/urls.py
+++ composite_handler/app/api/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 urlpatterns = [
     path("collections/<str:collection_id>", QueryCollectionAPIView.as_view()),
     path(
-        "collections/<str:collection_id>/members/<str:member_id>", QueryMemberAPIView.as_view()
+        "collections/<str:collection_id>/members/<str:member_id>",
+        QueryMemberAPIView.as_view(),
     ),
     path("customers/<int:customer>/queue", CollectionAPIView.as_view()),
 ]
