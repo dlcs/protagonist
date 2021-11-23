cf5687ac99d124d8923099518c5fee5a0963ae66
Daniel Grant <daniel.grant@digirati.com>
2021-11-23 13:53:04 +0000

    Updated API query path

--- composite_handler/app/api/urls.py
+++ composite_handler/app/api/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path("collections/<str:collection_id>", QueryCollectionAPIView.as_view()),
     path(
-        "collections/<str:collection_id>/<str:member_id>", QueryMemberAPIView.as_view()
+        "collections/<str:collection_id>/members/<str:member_id>", QueryMemberAPIView.as_view()
     ),
     path("customers/<int:customer>/queue", CollectionAPIView.as_view()),
 ]
