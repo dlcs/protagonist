b5f611a887022d577d52014d1f762d253f3474ae
Donald Gray <donald.gray@digirati.com>
2021-12-21 11:40:23 +0000

    Add DecodeBase64 helper method

--- DLCS.Core/Strings/StringX.cs
+++ DLCS.Core/Strings/StringX.cs
@@ -1,4 +1,7 @@
-﻿namespace DLCS.Core.Strings
+﻿using System;
+using System.Text;
+
+namespace DLCS.Core.Strings
 {
     public static class StringX
     {
@@ -8,5 +11,15 @@
         /// <param name="str">String to check</param>
         /// <returns>true if string contains content; else false</returns>
         public static bool HasText(this string str) => !string.IsNullOrWhiteSpace(str);
+
+        /// <summary>
+        /// Decode base64 encoded string back to UTF8 representation
+        /// </summary>
+        /// <param name="encoded">Base64 encoded string</param>
+        /// <returns>Decoded string</returns>
+        public static string DecodeBase64(this string encoded)
+            => encoded.HasText()
+                ? Encoding.UTF8.GetString(Convert.FromBase64String(encoded))
+                : encoded;
     }
 }
