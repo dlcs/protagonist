be4dfd6315dbcf7d0bb6be291b12c232700d8dcd
Daniel Grant <daniel.grant@digirati.com>
2022-01-13 15:25:57 +0000

    Implemented composite handler batching to allow larger PDF's to be split into multiple DLCS batches

--- composite_handler/app/common/models.py
+++ composite_handler/app/common/models.py
@@ -20,8 +20,19 @@ class Member(models.Model):
     json_data = models.JSONField()
     status = models.CharField(max_length=21, default="PENDING")
     image_count = models.IntegerField(null=True, blank=True)
-    dlcs_uri = models.TextField(null=True, blank=True)
     error = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    last_updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class DLCSBatch(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    json_data = models.JSONField()
+    uri = models.TextField(null=True, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     last_updated_date = models.DateTimeField(auto_now=True)
 
