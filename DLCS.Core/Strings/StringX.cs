9b3420a38bf92ee2c31fc2726091c62f4b4a80e0
tomcrane <tom@nanoplanet.com>
2022-04-07 08:56:21 +0100

    resolve conflicts in StringX

--- DLCS.Core/Strings/StringX.cs
+++ DLCS.Core/Strings/StringX.cs
@@ -11,6 +11,16 @@ namespace DLCS.Core.Strings
         /// <param name="str">String to check</param>
         /// <returns>true if string contains content; else false</returns>
         public static bool HasText(this string str) => !string.IsNullOrWhiteSpace(str);
+
+        /// <summary>
+        /// Decode base64 encoded string back to UTF8 representation
+        /// </summary>
+        /// <param name="encoded">Base64 encoded string</param>
+        /// <returns>Decoded string</returns>
+        public static string DecodeBase64(this string encoded)
+            => encoded.HasText()
+                ? Encoding.UTF8.GetString(Convert.FromBase64String(encoded))
+                : encoded;
         
         /// <summary>
         /// converts "Some list of strings" to "someListOfStrings"
