ad1fff9ae409c9b3ff6b388502150b5096b71d0f
Donald Gray <donald.gray@digirati.com>
2022-07-22 16:30:09 +0100

    Add systemsmanager to API startup

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
@@ -33,6 +34,17 @@ namespace API
                 .UseSerilog((hostingContext, loggerConfiguration)
                     => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration)
                 )
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    if (context.HostingEnvironment.IsProduction())
+                    {
+                        builder.AddSystemsManager(configurationSource =>
+                        {
+                            configurationSource.Path = "/protagonist/";
+                            configurationSource.ReloadAfter = TimeSpan.FromMinutes(90);
+                        });
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
