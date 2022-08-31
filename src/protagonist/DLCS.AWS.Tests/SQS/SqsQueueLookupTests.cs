fca04318311dc6f8a9821c656e53e2ebfe644e08
Donald Gray <donald.gray@digirati.com>
2022-08-31 12:52:53 +0100

    Add classes for sending notifications

---
+++ src/protagonist/DLCS.AWS.Tests/SQS/SqsQueueLookupTests.cs
@@ -0,0 +1,67 @@
+﻿using DLCS.AWS.Settings;
+using DLCS.AWS.SQS;
+using DLCS.Model.Assets;
+using Microsoft.Extensions.Options;
+
+namespace DLCS.AWS.Tests.SQS;
+
+public class SqsQueueLookupTests
+{
+    private readonly SqsQueueLookup sut;
+
+    public SqsQueueLookupTests()
+    {
+        sut = new SqsQueueLookup(Options.Create(new AWSSettings
+        {
+            Region = "eu-west-1",
+            SQS = new SQSSettings
+            {
+                PriorityImageQueueName = "test-priority",
+                ImageQueueName = "test-image",
+                TimebasedQueueName = "test-timebased"
+            }
+        }));
+    }
+    
+    [Fact]
+    public void GetQueueNameForFamily_Image_Correct()
+    {
+        // Act
+        var result = sut.GetQueueNameForFamily(AssetFamily.Image);
+        
+        // Assert
+        result.Should().Be("test-image");
+    }
+    
+    [Fact]
+    public void GetQueueNameForFamily_Image_Priority_Correct()
+    {
+        // Act
+        var result = sut.GetQueueNameForFamily(AssetFamily.Image, true);
+        
+        // Assert
+        result.Should().Be("test-priority");
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void GetQueueNameForFamily_Timebased_Correct_IgnoresPriority(bool priority)
+    {
+        // Act
+        var result = sut.GetQueueNameForFamily(AssetFamily.Timebased, priority);
+        
+        // Assert
+        result.Should().Be("test-timebased");
+    }
+
+    [Fact]
+    public void GetQueueNameForFamily_File_Throws()
+    {
+        // Arrange
+        Action action = () => sut.GetQueueNameForFamily(AssetFamily.File);
+        
+        // Assert
+        action.Should().Throw<InvalidOperationException>();
+    }
+}
---
+++ src/protagonist/DLCS.AWS/SQS/SqsQueueSender.cs
@@ -0,0 +1,49 @@
+﻿using Amazon.SQS;
+using Microsoft.Extensions.Logging;
+
+namespace DLCS.AWS.SQS;
+
+/// <summary>
+/// Implementation of <see cref="IQueueSender"/> using Sqs for backing queue
+/// </summary>
+public class SqsQueueSender : IQueueSender
+{
+    private readonly IAmazonSQS client;
+    private readonly SqsQueueUtilities queueUtilities;
+    private readonly ILogger<SqsQueueSender> logger;
+    private readonly Dictionary<string, string> nameUrlLookup = new();
+
+    public SqsQueueSender(IAmazonSQS client, SqsQueueUtilities queueUtilities, ILogger<SqsQueueSender> logger)
+    {
+        this.client = client;
+        this.queueUtilities = queueUtilities;
+        this.logger = logger;
+    }
+
+    public async Task<bool> QueueMessage(string queueName, string messageContents,
+        CancellationToken cancellationToken = default)
+    {
+        var queueUrl = await GetQueueUrl(queueName, cancellationToken);
+        try
+        {
+            var result = await client.SendMessageAsync(queueUrl, messageContents, cancellationToken);
+
+            var statusCode = (int)result.HttpStatusCode;
+            return statusCode is >= 200 and < 300;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error sending message to {QueueName}", queueName);
+            return false;
+        }
+    }
+
+    private async ValueTask<string> GetQueueUrl(string queueName, CancellationToken cancellationToken)
+    {
+        if (nameUrlLookup.TryGetValue(queueName, out var dictQueueUrl)) return dictQueueUrl;
+
+        var queueUrl = await queueUtilities.GetQueueUrl(queueName, cancellationToken);
+        nameUrlLookup[queueName] = queueUrl;
+        return queueUrl;
+    }
+}
