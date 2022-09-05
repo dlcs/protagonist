93dbd9188bdcaa7818b9991e12dd76c392efe487
Donald Gray <donald.gray@digirati.com>
2022-09-05 14:40:39 +0100

    Add ChangeManager class for applying updates to new/existing objects

---
+++ src/protagonist/DLCS.Core/ChangeManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+
+namespace DLCS.Core;
+
+/// <summary>
+/// Contains methods for applying changes to existing objects
+/// </summary>
+public static class ChangeManager
+{
+    /// <summary>
+    /// Replace nulls in toUpdate object with values from default object 
+    /// </summary>
+    /// <param name="toUpdate">Object to update</param>
+    /// <param name="defaultObject">Object containing default values.</param>
+    /// <typeparam name="T">Type of object</typeparam>
+    public static void DefaultNullProperties<T>(this T toUpdate, T defaultObject)
+        where T : class
+    {
+        var properties = toUpdate.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach(var prop in properties)
+        {
+            if (prop.GetValue(toUpdate) == null)
+            {
+                prop.SetValue(toUpdate, prop.GetValue(defaultObject));
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Update toUpdate with any non-null values from candidateChanges that differ to existing values.
+    /// </summary>
+    /// <param name="toUpdate">Object to update</param>
+    /// <param name="candidateChanges">Object containing candidate changes, any not set are null</param>
+    /// <typeparam name="T">Type of object</typeparam>
+    /// <returns>Number of properties update</returns>
+    /// <remarks>
+    /// Example use case for this is when toUpdate has been loaded from DB and candidateChanges has been passed in via
+    /// an HTTP request. Any non-null values in candidateChanges will have been set in request and should override those
+    /// already set on object
+    /// </remarks>
+    public static int ApplyChanges<T>(this T toUpdate, T candidateChanges)
+        where T : class
+    {
+        var properties = toUpdate.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var changesApplied = 0;
+        foreach (var prop in properties)
+        {
+            // Null candidate value can be ignored
+            var candidateValue = prop.GetValue(candidateChanges);
+            if (candidateValue == null) continue;
+            
+            var existingValue = prop.GetValue(toUpdate);
+            if (existingValue != candidateValue)
+            {
+                changesApplied++;
+                prop.SetValue(toUpdate, candidateValue);
+            }
+        }
+
+        return changesApplied;
+    }
+}
