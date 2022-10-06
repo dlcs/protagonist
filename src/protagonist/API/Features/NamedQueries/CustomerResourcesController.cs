3fd65f4b1ac37279768a10265bb7a893730e61c2
Donald Gray <donald.gray@digirati.com>
2022-10-06 17:25:36 +0100

    Add stub NQ to api

---
+++ src/protagonist/API/Features/NamedQueries/CustomerResourcesController.cs
@@ -0,0 +1,42 @@
+﻿using API.Infrastructure;
+using API.Settings;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace API.Features.NamedQueries;
+
+/// <summary>
+/// Controller for handling requests for customer resources
+/// </summary>
+[Route("/customers/{customerId}/resources")]
+[ApiController]
+public class CustomerResourcesController : HydraController
+{
+    /// <inheritdoc />
+    public CustomerResourcesController(IOptions<ApiSettings> settings, IMediator mediator) 
+        : base(settings.Value, mediator)
+    {
+    }
+    
+    /// <summary>
+    /// DELETE /customers/{customerId}/resources/pdf/{queryName}?args=
+    ///
+    /// Deletes PDF generates for queryName using specified arguments
+    /// </summary>
+    /// <param name="customerId">CustomerId to delete PDF from</param>
+    /// <param name="queryName">Name of named query</param>
+    /// <param name="args">Named query arguments</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    [HttpDelete]
+    [Route("pdf/{queryName}")]
+    public async Task<IActionResult> DeletePdf(
+        [FromRoute] int customerId,
+        [FromRoute] string queryName,
+        [FromQuery] string args,
+        CancellationToken cancellationToken = default)
+    {
+        
+    }
+}
---
+++ src/protagonist/API/Features/NamedQueries/Requests/DeletePdf.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace API.Features.NamedQueries.Requests;
+
+/// <summary>
+/// Handler for deleting PDF for named query
+/// </summary>
+public class DeletePdf : IRequest<bool?>
+{
+    public string QueryName { get; }
+    public string Args { get; }
+
+    public DeletePdf(string queryName, string args)
+    {
+        QueryName = queryName;
+        Args = args;
+    }
+}
+
