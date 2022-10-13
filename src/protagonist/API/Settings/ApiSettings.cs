8c56fe528f8ca1f60e0767575935816b94a0a4b8
Donald Gray <donald.gray@digirati.com>
2022-10-13 11:04:48 +0100

    Remove unused settings from API settings
    Cannot see this used so removing. Comment of "will be removed from API
    project once the Image refactor is done" seems to be complete

--- src/protagonist/API/Settings/ApiSettings.cs
+++ src/protagonist/API/Settings/ApiSettings.cs
@@ -1,5 +1,4 @@
-﻿using API.Client;
-using DLCS.AWS.Settings;
+﻿using DLCS.AWS.Settings;
 using DLCS.Core.Settings;
 
 namespace API.Settings;
@@ -12,10 +11,7 @@ public class ApiSettings
     public DlcsSettings DLCS { get; set; }
     
     public AWSSettings AWS { get; set; }
-    
-    // This will be removed from the API project once the Image refactor is done
-    public ApiClientSettings API { get; set; }
-    
+
     public string PathBase { get; set; }
     
     public string Salt { get; set; }
