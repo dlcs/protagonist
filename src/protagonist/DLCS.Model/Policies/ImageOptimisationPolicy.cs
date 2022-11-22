6f7e253a8e10c2745d2b0d14d52df0aa48a4d748
Donald Gray <donald.gray@digirati.com>
2022-11-22 15:18:07 +0000

    ImageOptimisationPolicy gains customer + global

--- src/protagonist/DLCS.Model/Policies/ImageOptimisationPolicy.cs
+++ src/protagonist/DLCS.Model/Policies/ImageOptimisationPolicy.cs
@@ -2,10 +2,25 @@
 
 namespace DLCS.Model.Policies;
 
+/// <summary>
+/// Represents set of instructions on how to process and image.
+/// The format of the 'TechnicalDetails' will depend on the downstream system carrying out the changes.
+/// e.g. they may be ElasticTranscoder presets, or a set of instructions for kdu
+/// </summary>
 [DebuggerDisplay("{Name}")]
 public class ImageOptimisationPolicy
 {
     public string Id { get; set; }
     public string Name { get; set; }
     public string[] TechnicalDetails { get; set; }
+    
+    /// <summary>
+    /// Customer that this optimisation policy is for
+    /// </summary>
+    public int? Customer { get; set; }
+    
+    /// <summary>
+    /// If true, optimisation policy is for all customers
+    /// </summary>
+    public bool Global { get; set; }
 }
