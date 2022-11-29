6ddedf591e6498a7a56da8bf51e35e0128406297
Donald Gray <donald.gray@digirati.com>
2022-11-29 10:31:12 +0000

    Add thumbs delivery-channel and channel helpers

---
+++ src/protagonist/DLCS.Model.Tests/Assets/AssetDeliveryChannelsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using DLCS.Model.Assets;
+using FluentAssertions;
+using Xunit;
+
+namespace DLCS.Model.Tests.Assets;
+
+public class AssetDeliveryChannelsTests
+{
+    [Fact]
+    public void HasDeliveryChannel_False_IfChannelsNull()
+    {
+        var asset = new Asset();
+
+        asset.HasDeliveryChannel("anything").Should().BeFalse();
+    }
+    
+    [Fact]
+    public void HasDeliveryChannel_False_IfChannelsEmpty()
+    {
+        var asset = new Asset { DeliveryChannel = Array.Empty<string>() };
+
+        asset.HasDeliveryChannel("anything").Should().BeFalse();
+    }
+    
+    [Fact]
+    public void HasDeliveryChannel_Throws_IfUnknown()
+    {
+        var asset = new Asset { DeliveryChannel = new[] { "iiif-img" } };
+
+        Action action = () => asset.HasDeliveryChannel("anything");
+
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+    
+    [Theory]
+    [InlineData(AssetDeliveryChannels.File)]
+    [InlineData(AssetDeliveryChannels.Image)]
+    [InlineData(AssetDeliveryChannels.Timebased)]
+    [InlineData(AssetDeliveryChannels.Thumbs)]
+    public void HasDeliveryChannel_True(string channel)
+    {
+        var asset = new Asset { DeliveryChannel = AssetDeliveryChannels.All };
+
+        asset.HasDeliveryChannel(channel).Should().BeTrue();
+    }
+}
--- src/protagonist/DLCS.Model/Assets/AssetDeliveryChannels.cs
+++ src/protagonist/DLCS.Model/Assets/AssetDeliveryChannels.cs
@@ -1,18 +1,38 @@
-﻿namespace DLCS.Model.Assets;
+﻿using System;
+using System.Linq;
+using DLCS.Core.Collections;
+
+namespace DLCS.Model.Assets;
 
 public static class AssetDeliveryChannels
 {
     public const string Image = "iiif-img";
     public const string Timebased = "iiif-av";
     public const string File = "file";
+    public const string Thumbs = "thumbs";
 
     /// <summary>
     /// All possible delivery channels
     /// </summary>
-    public static string[] All { get; } = { File, Timebased, Image };
+    public static string[] All { get; } = { File, Timebased, Image, Thumbs };
 
     /// <summary>
     /// All possible delivery channels as a comma-delimited string
     /// </summary>
-    public static string AllString = string.Join(',', All);
+    public static readonly string AllString = string.Join(',', All);
+
+    /// <summary>
+    /// Check if asset has specified deliveryChannel
+    /// </summary>
+    public static bool HasDeliveryChannel(this Asset asset, string deliveryChannel)
+    {
+        if (asset.DeliveryChannel.IsNullOrEmpty()) return false;
+        if (!All.Contains(deliveryChannel))
+        {
+            throw new ArgumentOutOfRangeException(nameof(deliveryChannel), deliveryChannel,
+                $"Acceptable delivery-channels are: {AllString}");
+        }
+
+        return asset.DeliveryChannel.Contains(deliveryChannel);
+    }
 }
