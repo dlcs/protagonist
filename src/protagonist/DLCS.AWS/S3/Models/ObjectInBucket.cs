befb67187d46a443aa708c72435d2d3ae4254b89
Donald Gray <donald.gray@digirati.com>
2023-03-17 15:42:31 +0000

    Add comparison operators to objectInBucket

--- src/protagonist/DLCS.AWS/S3/Models/ObjectInBucket.cs
+++ src/protagonist/DLCS.AWS/S3/Models/ObjectInBucket.cs
@@ -29,6 +29,36 @@ public class ObjectInBucket
     /// <param name="key">New Key to use.</param>
     /// <returns>New ObjectInBucket object, same Bucket property but new Key.</returns>
     public ObjectInBucket CloneWithKey(string key) => new(Bucket, key);
+    
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((ObjectInBucket)obj);
+    }
+    
+    public override int GetHashCode() => HashCode.Combine(Bucket, Key);
+
+    public static bool operator ==(ObjectInBucket? objectInBucket1, ObjectInBucket? objectInBucket2)
+    {
+        if (objectInBucket1 is null)
+        {
+            return objectInBucket2 is null;
+        }
+        
+        if (objectInBucket2 is null)
+        {
+            return false;
+        }
+        
+        return objectInBucket1.Equals(objectInBucket2);
+    }
+
+    public static bool operator !=(ObjectInBucket objectInBucket1, ObjectInBucket objectInBucket2) 
+        => !(objectInBucket1 == objectInBucket2);
 
     public override string ToString() => $"{Bucket}:::{Key}";
+    
+    protected bool Equals(ObjectInBucket other) => Bucket == other.Bucket && Key == other.Key;
 }
