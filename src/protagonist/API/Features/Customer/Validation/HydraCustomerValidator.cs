8836971073f7b48cb68152e5c7d12540522500d3
griffri <Griffri.Melville@digirati.com>
2023-07-03 17:31:00 +0100

    Validate the Hydra customer model using Fluent

--- src/protagonist/API/Features/Customer/Validation/HydraCustomerValidator.cs
+++ src/protagonist/API/Features/Customer/Validation/HydraCustomerValidator.cs
@@ -1,53 +1,43 @@
-using System.Collections.Generic;
 using DLCS.Core.Strings;
+using FluentValidation;
 
-namespace API.Features.Customer.Validation;
+namespace API.Features.Customer.Validation; 
 
 /// <summary>
 /// Performs basic checks on a new POSTed or PUT Hydra Customer.
 /// Does not perform data integrity/constraint checks.
 /// </summary>
-public static class HydraCustomerValidator
+public class HydraCustomerValidator : AbstractValidator<DLCS.HydraModel.Customer>
 {
-    /// <summary>
-    /// Validates a customer is ready to be passed to the Customer repository
-    /// </summary>
-    /// <param name="newCustomer">The new Hydra Customer</param>
-    /// <returns>A list of string error messages</returns>
-    public static string[] GetNewHydraCustomerErrors(DLCS.HydraModel.Customer newCustomer)
+    public HydraCustomerValidator()
     {
-        var errorList = new List<string>();
-        if (newCustomer.ModelId > 0)
-        {
-            errorList.Add($"DLCS must allocate customer id, but id {newCustomer.ModelId} was supplied.");
-        }
-
-        if (string.IsNullOrEmpty(newCustomer.Name))
-        {
-            errorList.Add($"A new customer must have a name (url part).");
-        }
-            
-        if (string.IsNullOrEmpty(newCustomer.DisplayName))
-        {
-            errorList.Add($"A new customer must have a Display name (label).");
-        }
-            
-        if (newCustomer.Administrator == true)
-        {
-            errorList.Add($"You can't attempt to create an Administrator customer.");
-        }
-
-        if (newCustomer.Keys.HasText())
-        {
-            errorList.Add( $"You can't supply API Keys at customer creation time.");
-        }
+        RuleFor(c => c.ModelId).Empty()
+            .WithMessage(c => $"DLCS must allocate customer id, but id {c.ModelId} was supplied.");
         
-        if (Enum.GetNames(typeof(DLCS.HydraModel.Customer.ReservedNames)).Any(n =>
-                String.Equals(n, newCustomer.Name, StringComparison.CurrentCultureIgnoreCase)))
-        {
-            errorList.Add($"Name field [{newCustomer.Name}] cannot be a reserved word.");
-        }
-
-        return errorList.ToArray();
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .WithMessage("A new customer must have a name (url part).");
+        
+        RuleFor(c => c.DisplayName)
+            .NotEmpty()
+            .WithMessage("A new customer must have a Display name (label)..");
+        
+        RuleFor(c => c.Administrator)
+            .NotEqual(true)
+            .WithMessage("You can't attempt to create an Administrator customer.");
+        
+        RuleFor(c => c.Keys.HasText())
+            .Equal(false)
+            .WithMessage("You can't supply API Keys at customer creation time.");
+        
+        RuleFor(c => c.Name)
+            .Must(name => !IsReservedWord(name))
+            .WithMessage(c=> $"Name field [{c.Name}] cannot be a reserved word.");
+    }
+    
+    private bool IsReservedWord(string? customerName)
+    {
+        return Enum.GetNames(typeof(DLCS.HydraModel.Customer.ReservedNames)).Any(n =>
+            String.Equals(n, customerName, StringComparison.CurrentCultureIgnoreCase));
     }
 }
