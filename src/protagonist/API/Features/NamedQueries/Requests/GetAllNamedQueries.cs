6aaee045b48f89431890ec7f3145f3d0d60bb0bb
griffri <Griffri.Melville@digirati.com>
2023-07-13 14:30:34 +0100

    Add GetAllNamedQueries request

---
+++ src/protagonist/API/Features/NamedQueries/Requests/GetAllNamedQueries.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using DLCS.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Features.NamedQueries.Requests; 
+
+public class GetAllNamedQueries : IRequest<List<DLCS.Model.Assets.NamedQueries.NamedQuery>>
+{
+    public int CustomerId { get; }
+    
+    public GetAllNamedQueries(int id)
+    {
+        CustomerId = id;
+    }
+}
+
+public class GetAllNamedQueriesHandler : IRequestHandler<GetAllNamedQueries, List<DLCS.Model.Assets.NamedQueries.NamedQuery>>
+{
+    private readonly DlcsContext dbContext;
+    
+    public GetAllNamedQueriesHandler(DlcsContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    
+    public async Task<List<DLCS.Model.Assets.NamedQueries.NamedQuery>> Handle(GetAllNamedQueries request, CancellationToken cancellationToken)
+    {
+        return await dbContext.NamedQueries
+            .AsNoTracking()
+            .Where(x => x.Customer == request.CustomerId)
+            .ToListAsync(cancellationToken: cancellationToken);
+    }
+}
