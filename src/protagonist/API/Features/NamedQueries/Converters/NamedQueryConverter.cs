ac24314f881f3b4002dbb433e7d6ec12c2d7b0d5
griffri <Griffri.Melville@digirati.com>
2023-07-17 09:53:21 +0100

    Add Hydra converter class for named queries

---
+++ src/protagonist/API/Features/NamedQueries/Converters/NamedQueryConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Features.NamedQueries.Converters;
+
+public static class NamedQueryConverter
+{
+    public static DLCS.HydraModel.NamedQuery ToHydra(this DLCS.Model.Assets.NamedQueries.NamedQuery namedQuery, string baseUrl)
+    {
+        var hydraNamedQuery = new DLCS.HydraModel.NamedQuery(baseUrl, namedQuery.Customer, namedQuery.Id)
+        {
+            Name = namedQuery.Name,
+            Template = namedQuery.Template,
+            Global = namedQuery.Global
+        };
+        return hydraNamedQuery;
+    }
+}
