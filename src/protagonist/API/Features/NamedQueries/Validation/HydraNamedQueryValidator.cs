19dba901fcc9e8e46613098c63b337c9e3d0a47d
griffri <Griffri.Melville@digirati.com>
2023-07-20 16:56:33 +0100

    Add validator for Hydra Named Query model

---
+++ src/protagonist/API/Features/NamedQueries/Validation/HydraNamedQueryValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace API.Features.NamedQueries.Converters;
+
+public class HydraNamedQueryValidator : AbstractValidator<DLCS.HydraModel.NamedQuery>
+{
+    public HydraNamedQueryValidator()
+    {
+        RuleFor(nq => nq.ModelId)
+            .Empty()
+            .WithMessage(nq => $"DLCS must allocate named query id, but id {nq.ModelId} was supplied");
+        RuleFor(nq => nq.CustomerId)
+            .Empty()
+            .WithMessage("Should not include user id");
+        RuleFor(nq => nq.Name)
+            .NotEmpty()
+            .WithMessage("Name cannot be empty");
+        RuleFor(nq => nq.Template)
+            .NotEmpty()
+            .WithMessage("Template cannot be empty");
+    }
+}
