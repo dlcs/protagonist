40eaf7afdbb3e20ccc102d0107c3ca5cc88970a5
griffri <Griffri.Melville@digirati.com>
2023-07-26 11:48:38 +0100

    Add integration tests for NamedQueryController

---
+++ src/protagonist/API.Tests/Integration/NamedQueryTests.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using API.Client;
+using API.Tests.Integration.Infrastructure;
+using DLCS.Model.Assets.NamedQueries;
+using DLCS.Repository;
+using Hydra.Collections;
+using Microsoft.EntityFrameworkCore;
+using Test.Helpers.Integration;
+using Test.Helpers.Integration.Infrastructure;
+
+namespace API.Tests.Integration;
+
+[Trait("Category", "Integration")]
+[Collection(CollectionDefinitions.DatabaseCollection.CollectionName)]
+public class NamedQueryTests : IClassFixture<ProtagonistAppFactory<Startup>>
+{
+    private readonly HttpClient httpClient;
+    private readonly DlcsContext dlcsContext;
+
+    public NamedQueryTests(DlcsDatabaseFixture dbFixture, ProtagonistAppFactory<Startup> factory)
+    {
+        dlcsContext = dbFixture.DbContext;
+        httpClient = factory.ConfigureBasicAuthedIntegrationTestHttpClient(dbFixture, "API-Test");
+        dbFixture.CleanUp();
+    }
+    
+    [Fact]
+    public async Task Get_NamedQuery_200()
+    {
+        // Arrange
+        const int customerId = 96;
+        var path = $"customers/{customerId}/namedQueries";
+        var namedQuery = new NamedQuery()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Customer = customerId,
+            Name = "namedQueryTest",
+            Template = "test",
+            Global = false,
+        };
+        
+        await dlcsContext.NamedQueries.AddAsync(namedQuery);
+        await dlcsContext.SaveChangesAsync();
+        
+        // Act
+        var response = await httpClient.AsCustomer(customerId).GetAsync(Path.Combine(path, namedQuery.Id));
+        
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task Delete_NamedQuery_201()
+    {
+        // Arrange
+        const int customerId = 97;
+        var path = $"customers/{customerId}/namedQueries";
+        var namedQuery = new NamedQuery()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Customer = 97,
+            Name = "namedQueryTest",
+            Template = "test",
+            Global = false,
+        };
+        
+        await dlcsContext.NamedQueries.AddAsync(namedQuery);
+        await dlcsContext.SaveChangesAsync();
+        
+        // Act
+        var response = await httpClient.AsCustomer(customerId).DeleteAsync(Path.Combine(path, namedQuery.Id));
+        
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        var deletedNamedQuery = await dlcsContext.NamedQueries.SingleOrDefaultAsync(s => s.Id == namedQuery.Id);
+        deletedNamedQuery.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task Get_NamedQueries_200()
+    {
+        // Arrange
+        const int customerId = 98;
+        var path = $"customers/{customerId}/namedQueries";
+        
+        await dlcsContext.NamedQueries.AddTestNamedQuery("testNamedQuery1", customer: customerId, global: false);
+        await dlcsContext.NamedQueries.AddTestNamedQuery("testNamedQuery2", customer: 1);
+        await dlcsContext.NamedQueries.AddTestNamedQuery("testNamedQuery3", customer: 1, global: false);
+        await dlcsContext.SaveChangesAsync();
+        
+        // Act
+        var response = await httpClient.AsCustomer(customerId).GetAsync(path);
+        
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        var model = await response.ReadAsHydraResponseAsync<HydraCollection<NamedQuery>>();
+        model.Members.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public async Task Post_NamedQuery_201()
+    {
+        // Arrange
+        const int customerId = 99;
+        var path = $"customers/{customerId}/namedQueries";
+        
+        const string newNamedQueryJson = @"{
+          ""name"": ""namedQueryTest"",
+          ""template"": ""test"",
+        }";
+        
+        // Act
+        var content = new StringContent(newNamedQueryJson, Encoding.UTF8, "application/json");
+        var response = await httpClient.AsCustomer(customerId).PostAsync(path, content);
+        
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        
+        var foundNamedQuery = dlcsContext.NamedQueries.Single(nq => nq.Name == "namedQueryTest");
+        foundNamedQuery.Template.Should().Be("test");
+        foundNamedQuery.Customer.Should().Be(customerId);
+        foundNamedQuery.Global.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task Post_NamedQuery_400_IfUserCreatesGlobal()
+    {
+        // Arrange
+        const int customerId = 95;
+        var path = $"customers/{customerId}/namedQueries";
+        
+        const string newNamedQueryJson = @"{
+          ""name"": ""namedQueryTest"",
+          ""template"": ""test"",
+          ""global"": ""true""
+        }";
+
+        // Act
+        var content = new StringContent(newNamedQueryJson, Encoding.UTF8, "application/json");
+        var response = await httpClient.AsCustomer(customerId).PostAsync(path, content);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+}
