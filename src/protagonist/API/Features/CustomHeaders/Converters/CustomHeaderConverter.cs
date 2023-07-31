35336f094b9cedbb88a5573840d7be5020341d29
griffri <Griffri.Melville@digirati.com>
2023-07-31 15:36:39 +0100

    Add ToDlcsModel() function

--- src/protagonist/API/Features/CustomHeaders/Converters/CustomHeaderConverter.cs
+++ src/protagonist/API/Features/CustomHeaders/Converters/CustomHeaderConverter.cs
@@ -6,9 +6,23 @@ public static class CustomHeaderConverter
     {
         return new DLCS.HydraModel.CustomHeader(baseUrl, customHeader.Customer, customHeader.Id, false)
         {
+            SpaceId = customHeader.Space,
             Role = customHeader.Role,
             Key = customHeader.Key,
             Value = customHeader.Value,
+        };
+    }
+    
+    public static DLCS.Model.Assets.CustomHeaders.CustomHeader ToDlcsModel(this DLCS.HydraModel.CustomHeader hydraNamedQuery)
+    {
+        return new DLCS.Model.Assets.CustomHeaders.CustomHeader()
+        {
+            Id = hydraNamedQuery.ModelId,
+            Customer = hydraNamedQuery.CustomerId,
+            Space = hydraNamedQuery.SpaceId,
+            Role = hydraNamedQuery.Role,
+            Key = hydraNamedQuery.Key,
+            Value = hydraNamedQuery.Value
         };
     }
 }
