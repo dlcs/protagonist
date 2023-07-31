65471088ebfdacc525041158747a28fa4f80a362
griffri <Griffri.Melville@digirati.com>
2023-07-31 15:13:26 +0100

    Add parameters for ModelId and CustomerId in ToHydra()

--- src/protagonist/API/Features/CustomHeaders/Converters/CustomHeaderConverter.cs
+++ src/protagonist/API/Features/CustomHeaders/Converters/CustomHeaderConverter.cs
@@ -4,7 +4,7 @@ public static class CustomHeaderConverter
 {
     public static DLCS.HydraModel.CustomHeader ToHydra(this DLCS.Model.Assets.CustomHeaders.CustomHeader customHeader, string baseUrl)
     {
-        return new DLCS.HydraModel.CustomHeader(baseUrl, customHeader.Id, false)
+        return new DLCS.HydraModel.CustomHeader(baseUrl, customHeader.Customer, customHeader.Id, false)
         {
             Role = customHeader.Role,
             Key = customHeader.Key,
