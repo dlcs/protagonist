047f6681af95d94fd62cb8ab6257de978db8e333
griffri <Griffri.Melville@digirati.com>
2023-07-31 10:38:12 +0100

    Add converter class for custom headers

---
+++ src/protagonist/API/Features/CustomHeaders/Converters/CustomHeaderConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Features.CustomHeaders.Converters;
+
+public static class CustomHeaderConverter
+{
+    public static DLCS.HydraModel.CustomHeader ToHydra(this DLCS.Model.Assets.CustomHeaders.CustomHeader customHeader, string baseUrl)
+    {
+        return new DLCS.HydraModel.CustomHeader(baseUrl, customHeader.Id, false)
+        {
+            Role = customHeader.Role,
+            Key = customHeader.Key,
+            Value = customHeader.Value,
+        };
+    }
+}
