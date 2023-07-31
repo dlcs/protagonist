c0a53cfa618466d8290f42bb6e4a416d57ba0ef4
griffri <Griffri.Melville@digirati.com>
2023-07-31 15:17:02 +0100

    Add validator for custom headers

---
+++ src/protagonist/API/Features/CustomHeaders/Validation/CustomHeaderValidator.cs
@@ -0,0 +1,23 @@
+﻿ using FluentValidation;
+
+namespace API.Features.CustomHeaders.Validation;
+
+
+public class HydraCustomHeaderValidator : AbstractValidator<DLCS.HydraModel.CustomHeader>
+{
+    public HydraCustomHeaderValidator()
+    {
+        RuleFor(ch => ch.Id)
+            .Empty()
+            .WithMessage(ch => $"DLCS must allocate named query id, but id {ch.Id} was supplied");
+        RuleFor(nq => nq.CustomerId)
+            .Empty()
+            .WithMessage("Should not include user id");
+        RuleFor(ch => ch.Key)
+            .Empty()
+            .WithMessage("A key is required");
+        RuleFor(nq => nq.Value)
+            .NotEmpty()
+            .WithMessage("A value is required");
+    }
+}
