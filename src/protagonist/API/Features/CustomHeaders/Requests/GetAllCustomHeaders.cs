241574615eb7c3be87b9e0d05244fb58bba1f7bd
griffri <Griffri.Melville@digirati.com>
2023-07-31 11:06:20 +0100

    Add request class for retrieving the user's custom headers

---
+++ src/protagonist/API/Features/CustomHeaders/Requests/GetAllCustomHeaders.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using API.Features.NamedQueries.Requests;
+using API.Infrastructure.Requests;
+using DLCS.Model.Assets.CustomHeaders;
+using DLCS.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Features.CustomHeaders.Requests;
+
+public class GetAllCustomHeaders : IRequest<FetchEntityResult<IReadOnlyCollection<CustomHeader>>>
+{
+    public int CustomerId { get; }
+    
+    public GetAllCustomHeaders(int id)
+    {
+        CustomerId = id;
+    }
+}
+
+public class GetAllCustomHeadersHandler : IRequestHandler<GetAllCustomHeaders, FetchEntityResult<IReadOnlyCollection<CustomHeader>>>
+{
+    private readonly DlcsContext dbContext;
+    
+    public GetAllCustomHeadersHandler(DlcsContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    
+    public async Task<FetchEntityResult<IReadOnlyCollection<CustomHeader>>> Handle(
+        GetAllCustomHeaders request, 
+        CancellationToken cancellationToken)
+    {
+        var customHeaders = await dbContext.CustomHeaders
+            .AsNoTracking()
+            .Where(nq => nq.Customer == request.CustomerId)
+            .ToListAsync(cancellationToken);
+        
+        return FetchEntityResult<IReadOnlyCollection<CustomHeader>>.Success(customHeaders);
+    }
+}
