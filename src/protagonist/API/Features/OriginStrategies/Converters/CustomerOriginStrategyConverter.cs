7c3bcb75a5f24c21da69f320e451e1b6e1f6963e
griffri <Griffri.Melville@digirati.com>
2023-08-03 15:37:00 +0100

    Add converter for DLCS/Hydra customer origin strategy classes

---
+++ src/protagonist/API/Features/OriginStrategies/Converters/CustomerOriginStrategyConverter.cs
@@ -0,0 +1,43 @@
+﻿using DLCS.Core.Enum;
+using DLCS.Core.Strings;
+
+namespace API.Features.OriginStrategies.Converters;
+
+public static class CustomerOriginStrategyConverter
+{
+    public static DLCS.HydraModel.CustomerOriginStrategy ToHydra(this DLCS.Model.Customers.CustomerOriginStrategy originStrategy, string baseUrl)
+    {
+        var hydraOriginStrategy = new DLCS.HydraModel.CustomerOriginStrategy(baseUrl, originStrategy.Customer, originStrategy.Id)
+        {
+            Regex = originStrategy.Regex,
+            OriginStrategy = originStrategy.Strategy.GetDescription(),
+            Credentials = originStrategy.Credentials,
+            Optimised = originStrategy.Optimised,
+            Order = originStrategy.Order,
+        };
+        return hydraOriginStrategy;
+    }
+    
+    public static DLCS.Model.Customers.CustomerOriginStrategy ToDlcsModel(this DLCS.HydraModel.CustomerOriginStrategy hydraOriginStrategy)
+    {
+        var originStrategy = new DLCS.Model.Customers.CustomerOriginStrategy()
+        {
+            Id = hydraOriginStrategy.ModelId,
+            Customer = hydraOriginStrategy.CustomerId,
+            Regex = hydraOriginStrategy.Regex,
+            Credentials = hydraOriginStrategy.Credentials,
+        };
+        
+        if (hydraOriginStrategy.OriginStrategy.HasText())
+            originStrategy.Strategy = hydraOriginStrategy.OriginStrategy
+                .GetEnumFromString<DLCS.Model.Customers.OriginStrategyType>();
+        
+        if (hydraOriginStrategy.Optimised.HasValue) 
+            originStrategy.Optimised = hydraOriginStrategy.Optimised.Value;
+
+        if (hydraOriginStrategy.Order.HasValue) 
+            originStrategy.Order = hydraOriginStrategy.Order.Value;
+        
+        return originStrategy;
+    }
+}
