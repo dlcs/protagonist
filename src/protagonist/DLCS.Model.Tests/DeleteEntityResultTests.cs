b8dc3ce7b9a250ca5c3bbf66e3299859a6386c7e
Donald Gray <donald.gray@digirati.com>
2023-08-03 12:00:53 +0100

    Add DeleteEntityResult

---
+++ src/protagonist/DLCS.Model.Tests/DeleteEntityResultTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DLCS.Core;
+using DLCS.Model.Assets;
+using FluentAssertions;
+using Xunit;
+
+namespace DLCS.Model.Tests;
+
+public class DeleteEntityResultTests
+{
+    [Fact]
+    public void Ctor_Throws_IfDeletedStatus_WithNullEntity()
+    {
+        // Act
+        Action action = () => new DeleteEntityResult<Asset>(DeleteResult.Deleted);
+
+        // Assert
+        action.Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("Deleted Asset entity must be provided if status is 'Deleted'");
+    }
+    
+    [Theory]
+    [InlineData(DeleteResult.Conflict)]
+    [InlineData(DeleteResult.Error)]
+    [InlineData(DeleteResult.NotFound)]
+    public void Ctor_AllowsNullEntity_ForAllOtherStatus(DeleteResult result)
+    {
+        // Act
+        var deleteResult = new DeleteEntityResult<Asset>(result);
+
+        // Assert
+        deleteResult.Result.Should().Be(result);
+        deleteResult.DeletedEntity.Should().BeNull();
+    }
+}
---
+++ src/protagonist/DLCS.Model/DeleteEntityResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DLCS.Core;
+
+namespace DLCS.Model;
+
+/// <summary>
+/// Represents the result of a delete operation
+/// </summary>
+/// <typeparam name="T">Type of entity deleted</typeparam>
+public class DeleteEntityResult<T>
+    where T : class
+{
+    /// <summary>
+    /// Deleted entity - only available if delete was successful
+    /// </summary>
+    public T? DeletedEntity { get; }
+    
+    /// <summary>
+    /// The overall result of the attempted delete operation
+    /// </summary>
+    public DeleteResult Result { get; }
+
+    public DeleteEntityResult(DeleteResult deleteResult, T? deletedEntity = default)
+    {
+        if (deleteResult == DeleteResult.Deleted && deletedEntity is null)
+        {
+            throw new InvalidOperationException(
+                $"Deleted {typeof(T).Name} entity must be provided if status is 'Deleted'");
+        }
+        
+        Result = deleteResult;
+        DeletedEntity = deletedEntity;
+    }
+}
