37c36a7af82840298b5847c2cd45ff1e94015998
griffri <Griffri.Melville@digirati.com>
2023-08-04 14:24:46 +0100

    Add GetAllCustomerOriginStrategies request

---
+++ src/protagonist/API/Features/OriginStrategies/Requests/GetAllCustomerOriginStrategies.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using API.Infrastructure.Requests;
+using DLCS.Model.Customers;
+using DLCS.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Features.OriginStrategies.Requests;
+
+public class GetAllCustomerOriginStrategies : IRequest<FetchEntityResult<IReadOnlyCollection<CustomerOriginStrategy>>>
+{
+    public int CustomerId { get; }
+    
+    public GetAllCustomerOriginStrategies(int id)
+    {
+        CustomerId = id;
+    }
+}
+
+public class GetAllCustomerOriginStrategiesHandler : IRequestHandler<GetAllCustomerOriginStrategies, FetchEntityResult<IReadOnlyCollection<CustomerOriginStrategy>>>
+{
+    private readonly DlcsContext dbContext;
+    
+    public GetAllCustomerOriginStrategiesHandler(DlcsContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    
+    public async Task<FetchEntityResult<IReadOnlyCollection<CustomerOriginStrategy>>> Handle(
+        GetAllCustomerOriginStrategies request, 
+        CancellationToken cancellationToken)
+    {
+        var strategies = await dbContext.CustomerOriginStrategies
+            .AsNoTracking()
+            .Where(s => s.Customer == request.CustomerId)
+            .ToListAsync(cancellationToken);
+        
+        return FetchEntityResult<IReadOnlyCollection<CustomerOriginStrategy>>.Success(strategies);
+    }
+}
