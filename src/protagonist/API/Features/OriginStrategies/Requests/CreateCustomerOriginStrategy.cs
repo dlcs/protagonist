ef7d42ef080f6e77e225d8cc0081f54a1556f536
griffri <Griffri.Melville@digirati.com>
2023-08-04 16:28:26 +0100

    Add CreateCustomerOriginStrategy request WIP

---
+++ src/protagonist/API/Features/OriginStrategies/Requests/CreateCustomerOriginStrategy.cs
@@ -0,0 +1,59 @@
+﻿using API.Infrastructure.Requests;
+using DLCS.Core;
+using DLCS.Model.Customers;
+using DLCS.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Features.OriginStrategies.Requests;
+
+public class CreateCustomerOriginStrategy : IRequest<ModifyEntityResult<CustomerOriginStrategy>>
+{
+    public int CustomerId { get; }
+    
+    public CustomerOriginStrategy Strategy { get; }
+    
+    public CreateCustomerOriginStrategy(int customerId, CustomerOriginStrategy strategy)
+    {
+        CustomerId = customerId;
+        Strategy = strategy;
+    }
+}
+
+public class CreateCustomerOriginStrategyHandler : IRequestHandler<CreateCustomerOriginStrategy, ModifyEntityResult<CustomerOriginStrategy>>
+{  
+    private readonly DlcsContext dbContext;
+
+    public CreateCustomerOriginStrategyHandler(DlcsContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    
+    public async Task<ModifyEntityResult<CustomerOriginStrategy>> Handle(CreateCustomerOriginStrategy request, CancellationToken cancellationToken)
+    {
+        var existingStrategy = await dbContext.CustomerOriginStrategies.AsNoTracking().SingleOrDefaultAsync(
+            s => s.Customer == request.CustomerId && s.Regex == request.Strategy.Regex, cancellationToken);
+        
+        if (existingStrategy != null)
+        {
+            return ModifyEntityResult<CustomerOriginStrategy>.Failure("An origin strategy with the same regex already exists",
+                WriteResult.Conflict);
+        }
+        
+        var newStrategy = new CustomerOriginStrategy()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Customer = request.Strategy.Customer,
+            Regex = request.Strategy.Regex,
+            Strategy = request.Strategy.Strategy,
+            Credentials = request.Strategy.Credentials,
+            Optimised = request.Strategy.Optimised,
+            Order = request.Strategy.Order
+        };
+        
+        await dbContext.CustomerOriginStrategies.AddAsync(newStrategy, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken); 
+        
+        return ModifyEntityResult<CustomerOriginStrategy>.Success(newStrategy, WriteResult.Created);
+    }
+}
