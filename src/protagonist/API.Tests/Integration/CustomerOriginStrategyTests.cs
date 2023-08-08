6fb04efec01ed4845f1551acc69302211091e10d
griffri <Griffri.Melville@digirati.com>
2023-08-08 16:04:47 +0100

    Add Post_CustomerOriginStrategy_409_IfRegexAlreadyExists, Post_CustomerOriginStrategy_201, and Post_CustomerOriginStrategy_400_IfOptimisedWithoutS3AmbientStrategy tests

--- src/protagonist/API.Tests/Integration/CustomerOriginStrategyTests.cs
+++ src/protagonist/API.Tests/Integration/CustomerOriginStrategyTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using API.Tests.Integration.Infrastructure;
 using DLCS.Model.Customers;
@@ -51,5 +53,84 @@ public class CustomerOriginStrategyTests : IClassFixture<ProtagonistAppFactory<S
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task Post_CustomerOriginStrategy_201()
+    {
+        // Arrange
+        const int customerId = 91;
+        var path = $"customers/{customerId}/originStrategies";
+        const string newStrategyJson = @"{
+            ""originStrategy"": ""basic-http-authentication"",
+            ""credentials"": ""my-credentials"",
+            ""regex"": ""my-regex"",
+            ""order"": ""2""
+        }";
+        
+        // Act
+        var content = new StringContent(newStrategyJson, Encoding.UTF8, "application/json");
+        var response = await httpClient.AsCustomer(customerId).PostAsync(path, content);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        
+        var foundStrategy = dlcsContext.CustomerOriginStrategies.Single(s => s.Customer == customerId);
+        foundStrategy.Strategy.Should().Be(OriginStrategyType.BasicHttp);
+        foundStrategy.Credentials.Should().Be("my-credentials");
+        foundStrategy.Regex.Should().Be("my-regex");
+        foundStrategy.Order.Should().Be(2);
+    }
+    
+    [Fact]
+    public async Task Post_CustomerOriginStrategy_409_IfRegexAlreadyExists()
+    {
+        // Arrange
+        const int customerId = 92;
+        var path = $"customers/{customerId}/originStrategies";
+        var existingStrategy = new CustomerOriginStrategy()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Customer = customerId,
+            Regex = "my-regex",
+            Strategy = OriginStrategyType.BasicHttp
+        };
+        const string newStrategyJson = @"{
+            ""originStrategy"": ""basic-http-authentication"",
+            ""regex"": ""my-regex"",
+            ""order"": ""2""
+        }";
+        
+        await dlcsContext.CustomerOriginStrategies.AddAsync(existingStrategy);
+        await dlcsContext.SaveChangesAsync();
+        
+        // Act
+        var content = new StringContent(newStrategyJson, Encoding.UTF8, "application/json");
+        var response = await httpClient.AsCustomer(customerId).PostAsync(path, content);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
+    
+    [Fact]
+    public async Task Post_CustomerOriginStrategy_400_IfOptimisedWithoutS3AmbientStrategy()
+    {
+        // Arrange
+        const int customerId = 93;
+        var path = $"customers/{customerId}/originStrategies";
+        
+        const string newStrategyJson = @"{
+            ""originStrategy"": ""basic-http-authentication"",
+            ""regex"": ""my-regex"",
+            ""optimised"": ""true"",
+            ""order"": ""2""
+        }";
+        
+        // Act
+        var content = new StringContent(newStrategyJson, Encoding.UTF8, "application/json");
+        var response = await httpClient.AsCustomer(customerId).PostAsync(path, content);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
 }
