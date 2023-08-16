423de4640f9c34c568913e8ecdd3164530d98ef6
griffri <Griffri.Melville@digirati.com>
2023-08-16 15:00:42 +0100

    Ensure that credentials can only be used when the strategy is BasicHttp

--- src/protagonist/API/Features/OriginStrategies/Validators/HydraCustomerOriginStrategyValidator.cs
+++ src/protagonist/API/Features/OriginStrategies/Validators/HydraCustomerOriginStrategyValidator.cs
@@ -25,7 +25,15 @@ public class HydraCustomerOriginStrategyValidator : AbstractValidator<DLCS.Hydra
         RuleFor(s => s.Optimised)
             .NotEqual(true)
             .When(s => s.OriginStrategy != OriginStrategyType.S3Ambient.GetDescription())
-            .WithMessage("'Optimised' is only applicable if the origin strategy is s3-ambient");
+            .WithMessage("'Optimised' is only applicable when using s3-ambient as an origin strategy");
+        RuleFor(s => s.Credentials)
+            .NotEmpty()
+            .When(s => s.OriginStrategy == OriginStrategyType.BasicHttp.GetDescription())
+            .WithMessage("Credentials must be specified when using basic-http-authentication as an origin strategy");
+        RuleFor(s => s.Credentials)
+            .Empty()
+            .When(s => s.OriginStrategy != OriginStrategyType.BasicHttp.GetDescription())
+            .WithMessage("Credentials can only be specified when when using basic-http-authentication as an origin strategy");
         RuleFor(s => s.Regex)
             .NotEmpty()
             .WithMessage("Regex cannot be empty");
