12495aa921d7890f74aa9b29cfe1ae9e3f244a90
griffri <Griffri.Melville@digirati.com>
2023-08-18 09:20:26 +0100

    Update credentials in S3

--- src/protagonist/API/Features/OriginStrategies/Requests/UpdateCustomerOriginStrategy.cs
+++ src/protagonist/API/Features/OriginStrategies/Requests/UpdateCustomerOriginStrategy.cs
@@ -1,13 +1,16 @@
 ﻿using API.Features.Image.Requests;
 using API.Infrastructure.Requests;
 using DLCS.AWS.S3;
+using DLCS.AWS.S3.Models;
 using DLCS.Core;
 using DLCS.Core.Enum;
+using DLCS.Model.Auth;
 using DLCS.Model.Customers;
 using DLCS.Repository;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace API.Features.OriginStrategies.Requests;
 
@@ -16,7 +19,8 @@ public class UpdateCustomerOriginStrategy : IRequest<ModifyEntityResult<Customer
     public int CustomerId { get; }
     public string StrategyId { get; }
     public string? Regex { get; set; }
-    public string? Strategy { get; set; }
+    public string? Credentials { get; set; }
+    public OriginStrategyType? Strategy { get; set; }
     public int? Order { get; set; }
     public bool? Optimised { get; set; }
     
@@ -28,12 +32,12 @@ public class UpdateCustomerOriginStrategy : IRequest<ModifyEntityResult<Customer
 }
 
 public class UpdateCustomerOriginStrategyHandler : IRequestHandler<UpdateCustomerOriginStrategy, ModifyEntityResult<CustomerOriginStrategy>>
-{  
+{
     private readonly DlcsContext dbContext;
-    private readonly ILogger<HostAssetAtOriginHandler> logger;
     private readonly IBucketWriter bucketWriter;
     private readonly IStorageKeyGenerator storageKeyGenerator;
-    
+    private readonly JsonSerializerOptions jsonSettings = new(JsonSerializerDefaults.Web);
+
     public UpdateCustomerOriginStrategyHandler(
         DlcsContext dbContext,
         ILogger<HostAssetAtOriginHandler> logger,
@@ -41,42 +45,89 @@ public class UpdateCustomerOriginStrategyHandler : IRequestHandler<UpdateCustome
         IStorageKeyGenerator storageKeyGenerator)
     {
         this.dbContext = dbContext;
-        this.logger = logger;
         this.bucketWriter = bucketWriter;
         this.storageKeyGenerator = storageKeyGenerator;
     }
-    
+
     public async Task<ModifyEntityResult<CustomerOriginStrategy>> Handle(
-        UpdateCustomerOriginStrategy request, 
+        UpdateCustomerOriginStrategy request,
         CancellationToken cancellationToken)
     {
         var existingStrategy = await dbContext.CustomerOriginStrategies.SingleOrDefaultAsync(
-            s => s.Id == request.StrategyId && s.Customer == request.CustomerId, 
+            s => s.Id == request.StrategyId && s.Customer == request.CustomerId,
             cancellationToken);
         
         if (existingStrategy == null)
-        {
             return ModifyEntityResult<CustomerOriginStrategy>
                 .Failure($"Couldn't find an origin strategy with the id {request.StrategyId}", WriteResult.NotFound);
-        }
         
         if (!string.IsNullOrWhiteSpace(request.Regex))
+        {
+            var regexUsed = await dbContext.CustomerOriginStrategies.AnyAsync(
+                s => s.Customer == request.CustomerId && s.Regex == request.Regex && s.Id != existingStrategy.Id,
+                cancellationToken);
+
+            if (regexUsed)
+                return ModifyEntityResult<CustomerOriginStrategy>.Failure(
+                    "An origin strategy using the same regex already exists",
+                    WriteResult.Conflict);
+
             existingStrategy.Regex = request.Regex;
-        if (!string.IsNullOrWhiteSpace(request.Strategy))
-            existingStrategy.Strategy = request.Strategy.GetEnumFromString<OriginStrategyType>();
+        }
+        
+        if (request.Strategy.HasValue)
+            existingStrategy.Strategy = request.Strategy.Value;
+        
         if (request.Optimised.HasValue)
             existingStrategy.Optimised = request.Optimised.Value;
+       
         if (request.Order.HasValue)
             existingStrategy.Order = request.Order.Value;
         
         if (existingStrategy.Strategy != OriginStrategyType.S3Ambient && existingStrategy.Optimised)
             return ModifyEntityResult<CustomerOriginStrategy>
-                .Failure($"'Optimised' is only applicable if the origin strategy is s3-ambient", WriteResult.NotFound);
+                .Failure($"'Optimised' is only applicable when using s3-ambient as an origin strategy", WriteResult.FailedValidation);
         
-        await dbContext.SaveChangesAsync(cancellationToken);
+        if(existingStrategy.Strategy == OriginStrategyType.BasicHttp && string.IsNullOrWhiteSpace(request.Credentials))
+            return ModifyEntityResult<CustomerOriginStrategy>
+                .Failure($"Credentials must be specified when using basic-http-authentication as an origin strategy", WriteResult.FailedValidation);
+        
+        if (!string.IsNullOrWhiteSpace(request.Credentials))
+        {
+            if (!(!string.IsNullOrEmpty(request.Regex) && request.Strategy.HasValue && request.Optimised.HasValue && request.Order.HasValue))
+                return ModifyEntityResult<CustomerOriginStrategy>
+                    .Failure($"A full origin strategy object is required when updating credentials",
+                        WriteResult.FailedValidation);
 
+            if (existingStrategy.Strategy != OriginStrategyType.BasicHttp)
+                return ModifyEntityResult<CustomerOriginStrategy>
+                    .Failure($"Credentials can only be specified when using basic-http-authentication as an origin strategy",
+                        WriteResult.FailedValidation);
+            try
+            {
+                var credentials = JsonSerializer.Deserialize<BasicCredentials>(request.Credentials, jsonSettings);
+                
+                if(string.IsNullOrWhiteSpace(credentials?.User))
+                    return ModifyEntityResult<CustomerOriginStrategy>.Failure($"The credentials object requires an username", WriteResult.FailedValidation);
+                if(string.IsNullOrWhiteSpace(credentials?.Password))
+                    return ModifyEntityResult<CustomerOriginStrategy>.Failure($"The credentials object requires a password", WriteResult.FailedValidation);
+               
+                var credentialsJson = JsonSerializer.Serialize(credentials, jsonSettings);
+                var objectInBucket = storageKeyGenerator.GetOriginStrategyCredentialsLocation(request.CustomerId, existingStrategy.Id);
+                
+                await bucketWriter.WriteToBucket(objectInBucket, credentialsJson, "application/json", cancellationToken);
+                
+                existingStrategy.Credentials = objectInBucket.GetS3Uri().ToString();
+            }
+            catch (Exception e)
+            {
+                return ModifyEntityResult<CustomerOriginStrategy>.Failure($"Invalid credentials JSON: {e.Message}", WriteResult.FailedValidation);
+            }
+        }
+
+        await dbContext.SaveChangesAsync(cancellationToken);
         existingStrategy.Credentials = "xxx";
-        
+
         return ModifyEntityResult<CustomerOriginStrategy>.Success(existingStrategy);
     }
 }
