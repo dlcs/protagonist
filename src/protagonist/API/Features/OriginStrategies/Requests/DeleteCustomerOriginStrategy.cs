39b933ee97787a1de02f6e88e2fd2bb9b905a5bb
griffri <Griffri.Melville@digirati.com>
2023-08-18 13:30:12 +0100

    Add request for deleting customer origin strategies

---
+++ src/protagonist/API/Features/OriginStrategies/Requests/DeleteCustomerOriginStrategy.cs
@@ -0,0 +1,48 @@
+﻿using DLCS.Core;
+using DLCS.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Features.OriginStrategies.Requests;
+
+public class DeleteCustomerOriginStrategy : IRequest<ResultMessage<DeleteResult>>
+{
+    public int CustomerId { get; }
+    public string StrategyId { get; }
+
+    public DeleteCustomerOriginStrategy(int customerId, string strategyId)
+    {
+        CustomerId = customerId;
+        StrategyId = strategyId;
+    }
+    
+    public class DeleteCustomerOriginStrategyHandler : IRequestHandler<DeleteCustomerOriginStrategy, ResultMessage<DeleteResult>>
+    {
+        private readonly DlcsContext dbContext;
+
+        public DeleteCustomerOriginStrategyHandler(DlcsContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<ResultMessage<DeleteResult>> Handle(DeleteCustomerOriginStrategy request,
+            CancellationToken cancellationToken)
+        {
+            var strategy = await dbContext.CustomerOriginStrategies.SingleOrDefaultAsync(
+                s => s.Customer == request.CustomerId &&
+                      s.Id == request.StrategyId,
+                cancellationToken: cancellationToken);
+
+            if (strategy == null) 
+                return new ResultMessage<DeleteResult>(
+                    $"Deletion failed - origin strategy {request.StrategyId} was not found", DeleteResult.NotFound);
+
+            dbContext.CustomerOriginStrategies.Remove(strategy);
+            
+            await dbContext.SaveChangesAsync(cancellationToken);
+            
+            return new ResultMessage<DeleteResult>(
+                $"Origin strategy {request.StrategyId} successfully deleted", DeleteResult.Deleted);
+        }
+    }
+}
