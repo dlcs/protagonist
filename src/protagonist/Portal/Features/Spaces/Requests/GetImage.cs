90199bd3474472b2a1edc3651296b0c1a55b03b8
griffri <Griffri.Melville@digirati.com>
2023-10-11 15:58:01 +0100

    Add iif-net to Portal packages, add thumbnail gallery to image viewer

--- src/protagonist/Portal/Features/Spaces/Requests/GetImage.cs
+++ src/protagonist/Portal/Features/Spaces/Requests/GetImage.cs
@@ -1,28 +1,61 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using API.Client;
 using DLCS.HydraModel;
+using IIIF.ImageApi.V3;
+using IIIF.Serialisation;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Portal.Features.Spaces.Requests;
 
-public class GetImage : IRequest<Image>
+public class GetImage : IRequest<GetImageResult>
 {
     public int SpaceId { get; set; }
     public string ImageId { get; set; }
 }
 
-public class GetImageHandler : IRequestHandler<GetImage, Image>
+public class GetImageResult
 {
-    private readonly IDlcsClient dlcsClient;
+    public Image Image { get; set; }
+    public ImageService3? ImageService { get; set; }
+}
 
-    public GetImageHandler(IDlcsClient dlcsClient)
+public class GetImageHandler : IRequestHandler<GetImage, GetImageResult>
+{
+    private readonly ILogger<DlcsClient> logger;
+    private readonly IDlcsClient dlcsClient;
+    private readonly HttpClient httpClient;
+
+    public GetImageHandler(IDlcsClient dlcsClient, HttpClient httpClient, ILogger<DlcsClient> logger)
     {
+        this.logger = logger;
         this.dlcsClient = dlcsClient;
+        this.httpClient = httpClient;
     }
     
-    public async Task<Image> Handle(GetImage request, CancellationToken cancellationToken)
+    public async Task<GetImageResult> Handle(GetImage request, CancellationToken cancellationToken)
     {
-        return await dlcsClient.GetImage(request.SpaceId, request.ImageId);
+        var image = await dlcsClient.GetImage(request.SpaceId, request.ImageId);
+        var thumbnailService = await GetImageThumbnailService(image);
+        return new GetImageResult() { Image = image, ImageService = thumbnailService };
+    }
+    
+    private async Task<ImageService3?> GetImageThumbnailService(Image image)
+    {
+        try
+        {
+            var response = await httpClient.GetAsync($"{image.ThumbnailImageService}/info.json");
+            if (!response.IsSuccessStatusCode) return null;
+            return await response.Content.ReadFromJsonAsync<ImageService3>();
+        }
+        catch (Exception ex) 
+        {  
+            logger.LogError("Failed to deserialize thumbnail image service {ThumbnailImageService}", image.ThumbnailImageService);
+            return null;
+        }
     }
 }
--- src/protagonist/Portal/Pages/Images/Index.cshtml.cs
+++ src/protagonist/Portal/Pages/Images/Index.cshtml.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using DLCS.Core.Settings;
 using DLCS.HydraModel;
+using IIIF;
+using IIIF.ImageApi.V3;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
+using Portal.Features.Images.Requests;
 using Portal.Features.Spaces.Requests;
 
 namespace Portal.Pages.Images;
@@ -14,23 +18,24 @@ namespace Portal.Pages.Images;
 public class Index : PageModel
 {
     private readonly IMediator mediator;
-    
-    public DlcsSettings DlcsSettings { get; }
-
     public Image Image { get; set; }
+    public ImageService3? Thumbnails { get; set; }
     
-    public Index(
-        IMediator mediator,
-        IOptions<DlcsSettings> dlcsSettings,
-        ClaimsPrincipal currentUser)
+    public Index(IMediator mediator, ClaimsPrincipal currentUser)
     {
         this.mediator = mediator;
-        DlcsSettings = dlcsSettings.Value;
     }
 
     public async Task<IActionResult> OnGetAsync(int space, string image)
     {
-        Image = await mediator.Send(new GetImage {SpaceId = space, ImageId = image});
+        var imageResult = await mediator.Send(new GetImage{SpaceId = space, ImageId = image});
+        Image = imageResult.Image;
+        Thumbnails = imageResult.ImageService;
         return Page();
+    }
+    
+    public string CreateSrc(Size size)
+    {
+        return $"{Image.ThumbnailImageService}/full/{size.Width},{size.Height}/0/default.jpg";
     }
 }
