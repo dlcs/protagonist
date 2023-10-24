576c765e5de7ed2ea4396123c446c1f0d9cd2135
griffri <Griffri.Melville@digirati.com>
2023-10-24 13:34:48 +0100

    Include references in batch gallery, add processing/error icons

--- src/protagonist/Portal/Pages/Batches/Index.cshtml.cs
+++ src/protagonist/Portal/Pages/Batches/Index.cshtml.cs
@@ -16,6 +16,7 @@ namespace Portal.Pages.Batches;
 public class Index : PageModel
 {
     private readonly IMediator mediator;
+    private const int ImageReferenceMaxLength = 32;
     public Batch Batch { get; set; }
     public HydraCollection<Image> Images { get; set; }
     public Dictionary<string, string> Thumbnails { get; set; }
@@ -32,5 +33,18 @@ public class Index : PageModel
         Images = batchResult.Images;
         Thumbnails = batchResult.Thumbnails;
         return Page();
+    }
+    
+    public string GetImageReference(Image image)
+    {
+        var reference = string.Format("{0}/{1}/{2}/{3}/{4}/{5}",
+            image.String1 ?? string.Empty,
+            image.String2 ?? string.Empty,
+            image.String3 ?? string.Empty,
+            image.Number1,
+            image.Number2,
+            image.Number3);
+        
+        return reference;
     }
 }
