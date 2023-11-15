ad4e9e63b16287b79f15331be1348ef313e2c7d3
griffri <Griffri.Melville@digirati.com>
2023-11-15 14:50:35 +0000

    Add DateTime extension for getting the default time format

---
+++ src/protagonist/Portal/Extensions/DateTimeX.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Portal.Extensions;
+
+public static class DateTimeX
+{
+    private const string DefaultTimeFormat = "yyyy-MM-dd hh:mm:ss";
+    
+    /// <summary>
+    /// Returns a DateTime object as a string in a preset format
+    /// </summary>
+    public static string GetDefaultTime(this DateTime dateTime)
+    {
+        return dateTime.ToString(DefaultTimeFormat);
+    }
+}
