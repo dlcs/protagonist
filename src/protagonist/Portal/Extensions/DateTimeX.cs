493fee839a4207de859651a06bcd400db9b0bdf4
griffri <Griffri.Melville@digirati.com>
2023-11-16 10:19:35 +0000

    Add nullable overload for GetDefaultTime()

--- src/protagonist/Portal/Extensions/DateTimeX.cs
+++ src/protagonist/Portal/Extensions/DateTimeX.cs
@@ -5,6 +5,7 @@ namespace Portal.Extensions;
 public static class DateTimeX
 {
     private const string DefaultTimeFormat = "yyyy-MM-dd hh:mm:ss";
+    private const string DateNotFoundMessage = "Date not found";
     
     /// <summary>
     /// Returns a DateTime object as a string in a preset format
@@ -12,5 +13,11 @@ public static class DateTimeX
     public static string GetDefaultTime(this DateTime dateTime)
     {
         return dateTime.ToString(DefaultTimeFormat);
+    }
+    
+    /// <inheritdoc cref="GetDefaultTime(DateTime)"/>
+    public static string GetDefaultTime(this DateTime? dateTime)
+    {
+        return dateTime.HasValue ? dateTime.Value.ToString(DefaultTimeFormat) : DateNotFoundMessage;
     }
 }
