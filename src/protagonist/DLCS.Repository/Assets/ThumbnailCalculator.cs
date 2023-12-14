916010db02ee8fc1a386389c3e8fea8219751c4c
griffri <Griffri.Melville@digirati.com>
2023-12-14 16:40:38 +0000

    Fix issue where the thumbnail calculator would retrieve the wrong size

--- src/protagonist/DLCS.Repository/Assets/ThumbnailCalculator.cs
+++ src/protagonist/DLCS.Repository/Assets/ThumbnailCalculator.cs
@@ -25,13 +25,43 @@ public static class ThumbnailCalculator
 
     private static SizeCandidate GetLongestEdge(List<Size> sizes, ImageRequest imageRequest)
     {
+        // get the longest dimension of the requested size
+        var max = Math.Max(imageRequest.Size.Width ?? 0, imageRequest.Size.Height ?? 0);
+        
         if (imageRequest.Size.Width > 0 && imageRequest.Size.Height > 0)
         {
-            // We don't actually need to check imageRequest.Size.Confined (!w,h) because same logic applies...
-            var max = Math.Max(imageRequest.Size.Width ?? 0, imageRequest.Size.Height ?? 0);
-            return sizes.Select(s => s.MaxDimension).Contains(max)
-                ? new SizeCandidate(max)
-                : new SizeCandidate();
+            if (imageRequest.Size.Confined)
+            {
+                // Pick the first thumbnail size as a reference for shape
+                var shape = sizes.First().GetShape();
+                var confinedSizeFound = false;
+
+                switch (shape)
+                {
+                    // If this is a landscape image, max should match its width
+                    case ImageShape.Landscape:
+                        confinedSizeFound = sizes.Exists(s => s.Width == max && imageRequest.Size.Height >= s.Height);
+                        break;
+                    // For portrait images, max should match its height
+                    case ImageShape.Portrait:
+                        confinedSizeFound = sizes.Exists(s => s.Height == max && imageRequest.Size.Width >= s.Width);
+                        break;
+                    // Lastly, for square images, max should match both dimensions
+                    case ImageShape.Square:
+                        confinedSizeFound = sizes.Exists(s => s.Height == max && s.Width == max);
+                        break;
+                }
+                
+                if (confinedSizeFound) return new SizeCandidate(max);
+            }
+            
+            var foundExactSize = sizes.Exists(s => 
+                s.Height == imageRequest.Size.Height &&
+                s.Width == imageRequest.Size.Width);
+        
+            return foundExactSize ?
+                new SizeCandidate(max) :
+                new SizeCandidate();
         }
         
         if (imageRequest.Size.Max)
@@ -73,6 +103,7 @@ public static class ThumbnailCalculator
         // TODO - handle there being none "open"?
         
         var sizeCandidate = GetLongestEdge(sizes, imageRequest);
+        
         if (sizeCandidate.KnownSize)
         {
             // We have found a matching size, use that.
