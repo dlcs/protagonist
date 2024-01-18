d0535608ab4bc0fd64a228f4b34016726b70f258
griffri <Griffri.Melville@digirati.com>
2024-01-18 16:09:54 +0000

    Strip unused code

--- src/protagonist/API.Tests/Integration/ModifyAssetWithoutDeliveryChannelsTests.cs
+++ src/protagonist/API.Tests/Integration/ModifyAssetWithoutDeliveryChannelsTests.cs
@@ -1,63 +1,27 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using Amazon.S3;
-using API.Client;
-using API.Infrastructure.Messaging;
-using API.Tests.Integration.Infrastructure;
 using DLCS.Core.Types;
-using DLCS.HydraModel;
-using DLCS.Model.Assets;
-using DLCS.Model.Messaging;
-using DLCS.Repository;
-using DLCS.Repository.Entities;
-using DLCS.Repository.Messaging;
-using FakeItEasy;
-using Hydra.Collections;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Test.Helpers.Integration;
 using Test.Helpers.Integration.Infrastructure;
-using AssetFamily = DLCS.Model.Assets.AssetFamily;
-using ImageOptimisationPolicy = DLCS.Model.Policies.ImageOptimisationPolicy;
 
 namespace API.Tests.Integration;
 
 [Trait("Category", "Integration")]
-[Collection(StorageCollection.CollectionName)]
 public class ModifyAssetWithoutDeliveryChannelsTests : IClassFixture<ProtagonistAppFactory<Startup>>
 {
-    private readonly DlcsContext dbContext;
     private readonly HttpClient httpClient;
-    private static readonly IAssetNotificationSender NotificationSender = A.Fake<IAssetNotificationSender>();
-    private readonly IAmazonS3 amazonS3;
-    private static readonly IEngineClient EngineClient = A.Fake<IEngineClient>();
     
     public ModifyAssetWithoutDeliveryChannelsTests(
-        StorageFixture storageFixture, 
         ProtagonistAppFactory<Startup> factory)
     {
-        var dbFixture = storageFixture.DbFixture;
-        amazonS3 = storageFixture.LocalStackFixture.AWSS3ClientFactory();
-        
-        dbContext = dbFixture.DbContext;
-        
         httpClient = factory
-            .WithConnectionString(dbFixture.ConnectionString)
-            .WithLocalStack(storageFixture.LocalStackFixture)
             .WithTestServices(services =>
             {
-                services.AddSingleton<IAssetNotificationSender>(_ => NotificationSender);
-                services.AddScoped<IEngineClient>(_ => EngineClient);
                 services.AddAuthentication("API-Test")
                     .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
                         "API-Test", _ => { });
@@ -66,8 +30,6 @@ public class ModifyAssetWithoutDeliveryChannelsTests : IClassFixture<Protagonist
             {
                 AllowAutoRedirect = false
             });
-        
-        dbFixture.CleanUp();
     }
     
     [Fact]
@@ -75,23 +37,19 @@ public class ModifyAssetWithoutDeliveryChannelsTests : IClassFixture<Protagonist
     {
         // Arrange 
         var assetId = new AssetId(99, 1, $"{nameof(Patch_Asset_Fails_When_Delivery_Channels_Are_Disabled)}");
-
-        var testAsset = await dbContext.Images.AddTestAsset(assetId, family: AssetFamily.Image,
-            ref1: "I am string 1", origin: "https://images.org/image2.tiff");
-        await dbContext.SaveChangesAsync();
-
         var hydraImageBody = @"{
           ""@type"": ""Image"",
           ""string1"": ""I am edited"",
           ""wcDeliveryChannels"": [
                 ""iiif-img""
             ]
-        }";                        
-        // act
+        }";    
+        
+        // Act
         var content = new StringContent(hydraImageBody, Encoding.UTF8, "application/json");
         var response = await httpClient.AsCustomer(99).PatchAsync(assetId.ToApiResourcePath(), content);
         
-        // assert
+        // Assert
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
 
         var body = await response.Content.ReadAsStringAsync();
