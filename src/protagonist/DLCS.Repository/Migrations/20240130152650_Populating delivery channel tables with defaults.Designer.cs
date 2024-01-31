c5cb9ffd64c62eaca93881687c20d07f6a90efbd
jack.lewis <jack.lewis@digirati.com>
2024-01-31 10:33:05 +0000

    initial commit adding in data to delivery channel tables

--- src/protagonist/DLCS.Repository/Migrations/20240130152650_Populating delivery channel tables with defaults.Designer.cs
+++ src/protagonist/DLCS.Repository/Migrations/20240130152650_Populating delivery channel tables with defaults.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DLCS.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DLCS.Repository.Migrations
 {
     [DbContext(typeof(DlcsContext))]
-    partial class DlcsContextModelSnapshot : ModelSnapshot
+    [Migration("20240130152650_Populating delivery channel tables with defaults")]
+    partial class Populatingdeliverychanneltableswithdefaults
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
