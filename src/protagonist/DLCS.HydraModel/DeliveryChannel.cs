f4941e7772ddc359c50771e2786e53f08ec23373
griffri <Griffri.Melville@digirati.com>
2024-02-13 10:08:49 +0000

    Remove CustomerID, SpaceId, and ModelId. Change methods to GET, POST and PUT

--- src/protagonist/DLCS.HydraModel/DeliveryChannel.cs
+++ src/protagonist/DLCS.HydraModel/DeliveryChannel.cs
@@ -6,38 +6,9 @@ namespace DLCS.HydraModel;
 
 [HydraClass(typeof(DeliveryChannelClass),
     Description = "A delivery channel represents a way an asset on the DLCS can be served.",
-    UriTemplate = "/customers/{0}/defaultDeliveryChannels/{1}, /customers/{0}/spaces/{1}/defaultDeliveryChannels/{2}")]
+    UriTemplate = "")]
 public class DeliveryChannel : DlcsResource
 {
-    public DeliveryChannel()
-    {
-        
-    }
-    
-    public DeliveryChannel(string baseUrl, int customerId, string deliveryChannelId, int? spaceId)
-    {
-        ModelId = deliveryChannelId;
-        CustomerId = customerId;
-        SpaceId = spaceId;
-        if (spaceId.HasValue)
-        {
-            Init(baseUrl, false, customerId, spaceId, deliveryChannelId);
-        }
-        else
-        {
-            Init(baseUrl, false, customerId, deliveryChannelId);
-        }
-    }
-    
-    [JsonIgnore]
-    public int CustomerId { get; set; }
-    
-    [JsonIgnore]
-    public int? SpaceId { get; set; }
-    
-    [JsonIgnore]
-    public string? ModelId { get; set; }
-    
     [RdfProperty(Description = "The name of the DLCS delivery channel this is based on.",
         Range = Names.XmlSchema.String, ReadOnly = false, WriteOnly = false)]
     [JsonProperty(Order = 11, PropertyName = "channel")]
@@ -62,6 +33,6 @@ public class DeliveryChannelClass: Class
     {
         SupportedOperations = CommonOperations.GetStandardResourceOperations(
             operationId, "Delivery Channel", Id,
-            "GET", "PUT", "DELETE");
+            "GET", "POST", "PUT");
     }
 }
