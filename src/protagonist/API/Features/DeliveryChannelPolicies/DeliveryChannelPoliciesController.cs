d91cfbd008ac47878b74071c61d4d8ef0491deaf
griffri <Griffri.Melville@digirati.com>
2024-02-13 13:43:33 +0000

    Add HydraDeliveryChannelPolicyValidator, DeliveryChannelPolicy Hydra model, basic DeliveryChannelPoliciesController

---
+++ src/protagonist/API/Features/DeliveryChannelPolicies/DeliveryChannelPoliciesController.cs
@@ -0,0 +1,109 @@
+﻿
+using API.Features.DeliveryChannelPolicies.Validation;
+using API.Infrastructure;
+using API.Settings;
+using DLCS.HydraModel;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace API.Features.DeliveryChannelPolicies;
+
+/// <summary>
+/// DLCS REST API Operations for delivery channel policies
+/// </summary>
+[Route("/customers/{customerId}/deliveryChannelPolicies")]
+[ApiController]
+public class DeliveryChannelPoliciesController : HydraController
+{
+    public DeliveryChannelPoliciesController(
+        IMediator mediator,
+        IOptions<ApiSettings> options) : base(options.Value, mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetPolicyCollections(int customerId)
+    {
+        throw new NotImplementedException();     
+    }
+
+    [HttpGet("{channelId}")]
+    public async Task<IActionResult> GetPolicyCollection(int customerId, string channelId)
+    {
+        throw new NotImplementedException();
+    }    
+    
+    [HttpPost("{channelId}")]
+    public async Task<IActionResult> PostPolicy(
+        [FromRoute] int customerId,
+        [FromRoute] string channelId,
+        [FromBody] DeliveryChannelPolicy hydraDeliveryChannelPolicy,
+        [FromServices] HydraDeliveryChannelPolicyValidator validator,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await validator.ValidateAsync(hydraDeliveryChannelPolicy, 
+            policy => policy.IncludeRuleSets("default", "post"), cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return this.ValidationFailed(validationResult);
+        }
+        
+        throw new NotImplementedException();       
+    }
+    
+    [HttpGet("{channelId}/{policyId}")]
+    [ProducesResponseType(200, Type = typeof(DLCS.HydraModel.DeliveryChannelPolicy))]
+    public async Task<IActionResult> GetPolicy(
+        [FromRoute] int customerId,
+        [FromRoute] string channelId,
+        [FromRoute] string policyId)
+    { 
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost("{channelId}/{policyId}")]
+    public async Task<IActionResult> PutPolicy(
+        [FromRoute] int customerId,
+        [FromRoute] string channelId,
+        [FromRoute] string policyId,
+        [FromBody] DeliveryChannelPolicy hydraDeliveryChannelPolicy,
+        [FromServices] HydraDeliveryChannelPolicyValidator validator,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await validator.ValidateAsync(hydraDeliveryChannelPolicy, 
+            policy => policy.IncludeRuleSets("default", "put"), cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return this.ValidationFailed(validationResult);
+        }
+        
+        throw new NotImplementedException();     
+    }
+    
+    [HttpPatch("{channelId}/{policyId}")]
+    public async Task<IActionResult> PatchPolicy(
+        [FromRoute] int customerId,
+        [FromRoute] string channelId,
+        [FromRoute] string policyId,
+        [FromBody] DeliveryChannelPolicy hydraDeliveryChannelPolicy,
+        [FromServices] HydraDeliveryChannelPolicyValidator validator,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await validator.ValidateAsync(hydraDeliveryChannelPolicy, 
+            policy => policy.IncludeRuleSets("default", "patch"), cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return this.ValidationFailed(validationResult);
+        }
+        
+        throw new NotImplementedException();     
+    } 
+    
+    [HttpDelete("{channelId}/{policyId}")]
+    public async Task<IActionResult> DeletePolicy(int customerId, string channelId, string policyId)
+    {
+        throw new NotImplementedException();
+    } 
+}
---
+++ src/protagonist/API/Features/DeliveryChannelPolicies/Validation/HydraDeliveryChannelPolicyValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+
+namespace API.Features.DeliveryChannelPolicies.Validation;
+
+/// <summary>
+/// Validator for model sent to POST /deliveryChannelPolicies and PUT/PATCH /deliveryChannelPolicies/{id}
+/// </summary>
+public class HydraDeliveryChannelPolicyValidator : AbstractValidator<DLCS.HydraModel.DeliveryChannelPolicy>
+{
+    private readonly string[] allowedDeliveryChannels = {"iiif-img", "iiif-av", "thumbs"};
+    
+    public HydraDeliveryChannelPolicyValidator()
+    {
+        RuleFor(p => p.Id)
+            .Empty()
+            .WithMessage(p => $"DLCS must allocate named origin strategy id, but id {p.Id} was supplied");
+        RuleSet("post", () =>
+        {
+            RuleFor(c => c.Channel)
+                .Empty().WithMessage("'name' is required");         
+        });
+        RuleSet("put", () =>
+        {
+            RuleFor(c => c.Channel)
+                .NotEmpty().WithMessage("'name' is not permitted");         
+        });
+        RuleSet("patch", () =>
+        {
+            RuleFor(c => c.Channel)
+                .Empty().WithMessage("'Name' cannot be modified in a PATCH operation");         
+            RuleFor(c => c.Channel)
+                .Empty().WithMessage("'Channel' cannot be modified in a PATCH operation");         
+        });
+        RuleFor(p => p.Channel)
+            .Must(c => allowedDeliveryChannels.Contains(c))
+            .WithMessage(p => $"'{p.Channel}' is not a supported delivery channel");
+        RuleFor(p => p.PolicyModified)
+            .Empty().WithMessage(c => $"'policyModified' is not permitted");
+    }
+}
