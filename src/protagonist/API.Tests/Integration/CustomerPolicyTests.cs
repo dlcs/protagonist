741dc11a76196a5ed137fc1dd5d847e9715e4eac
griffri <Griffri.Melville@digirati.com>
2024-02-16 16:19:40 +0000

    Move delivery channel policy tests into a separate file

--- src/protagonist/API.Tests/Integration/CustomerPolicyTests.cs
+++ src/protagonist/API.Tests/Integration/CustomerPolicyTests.cs
@@ -93,37 +93,4 @@ public class CustomerPolicyTests : IClassFixture<ProtagonistAppFactory<Startup>>
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
-    
-    [Fact]
-    public async Task Get_DeliveryChannelPolicy_200()
-    {
-        // Arrange
-        var path = $"customers/99/deliveryChannelPolicies/thumbs/example-thumbs-policy";
-  
-        // Act
-        var response = await httpClient.AsCustomer(99).GetAsync(path);
-        
-        // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-        
-        var model = await response.ReadAsHydraResponseAsync<DeliveryChannelPolicy>();
-        model.Name.Should().Be("example-thumbs-policy");
-        model.DisplayName.Should().Be("Example Thumbnail Policy");
-        model.Channel.Should().Be("thumbs");
-        model.PolicyData.Should().Be("{[\"!1024,1024\",\"!400,400\",\"!200,200\",\"!100,100\"]}");
-    }
-    
-    [Fact]
-    public async Task Get_DeliveryChannelPolicy_404_IfNotFound()
-    {
-        // Arrange
-        var path = $"customers/99/deliveryChannelPolicies/thumbs/foofoo";
-
-        // Act
-        var response = await httpClient.AsCustomer(99).GetAsync(path);
-
-        // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
-    }
-    
 }
---
+++ src/protagonist/API.Tests/Integration/DeliveryChannelTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using API.Client;
+using API.Tests.Integration.Infrastructure;
+using DLCS.HydraModel;
+using DLCS.Repository;
+using Hydra.Collections;
+using Test.Helpers.Integration;
+using Test.Helpers.Integration.Infrastructure;
+
+namespace API.Tests.Integration;
+
+[Trait("Category", "Integration")]
+[Collection(CollectionDefinitions.DatabaseCollection.CollectionName)]
+public class DeliveryChannelTests : IClassFixture<ProtagonistAppFactory<Startup>>
+{
+    private readonly HttpClient httpClient;
+
+    public  DeliveryChannelTests(DlcsDatabaseFixture dbFixture, ProtagonistAppFactory<Startup> factory)
+    {
+        httpClient = factory.ConfigureBasicAuthedIntegrationTestHttpClient(dbFixture, "API-Test");
+        dbFixture.CleanUp();
+    }
+
+    [Fact]
+    public async Task Get_DeliveryChannelPolicy_200()
+    {
+        // Arrange
+        var path = $"customers/99/deliveryChannelPolicies/thumbs/example-thumbs-policy";
+  
+        // Act
+        var response = await httpClient.AsCustomer(99).GetAsync(path);
+        
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        var model = await response.ReadAsHydraResponseAsync<DeliveryChannelPolicy>();
+        model.Name.Should().Be("example-thumbs-policy");
+        model.DisplayName.Should().Be("Example Thumbnail Policy");
+        model.Channel.Should().Be("thumbs");
+        model.PolicyData.Should().Be("{[\"!1024,1024\",\"!400,400\",\"!200,200\",\"!100,100\"]}");
+    }
+    
+    [Fact]
+    public async Task Get_DeliveryChannelPolicy_404_IfNotFound()
+    {
+        // Arrange
+        var path = $"customers/99/deliveryChannelPolicies/thumbs/foofoo";
+
+        // Act
+        var response = await httpClient.AsCustomer(99).GetAsync(path);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
