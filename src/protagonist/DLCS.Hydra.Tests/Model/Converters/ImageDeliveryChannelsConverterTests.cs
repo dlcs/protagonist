f74f656df7fae203a0f89419f828703e1c1b1633
griffri <Griffri.Melville@digirati.com>
2024-02-26 17:13:35 +0000

    Add ImageDeliveryChannelsConverter work in progress and tests

---
+++ src/protagonist/DLCS.Hydra.Tests/Model/Converters/ImageDeliveryChannelsConverterTests.cs
@@ -0,0 +1,92 @@
+﻿using DLCS.HydraModel;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace DLCS.Hydra.Tests.Model.Converters;
+
+public class ImageDeliveryChannelsConverterTests
+{
+    [Fact]
+    public async Task DeliveryChannelsConverter_Supports_Basic_Channels()
+    {
+        var hydraAssetBody = @"{
+            ""@type"": ""Image"",
+            ""origin"": ""https://example.org/asset.tiff"",
+            ""family"": ""I"",
+            ""mediaType"": ""image/tiff"",
+            ""deliveryChannels"": [""iiif-img"",""thumbs"",""file""]
+        }";
+        
+        var deliveryChannel = JsonConvert.DeserializeObject<Image>(hydraAssetBody);
+        
+        deliveryChannel!.DeliveryChannels!.Length.Should().Be(3);
+        deliveryChannel!.DeliveryChannels!.Should().BeEquivalentTo(new DeliveryChannel[]
+        {
+            new()
+            {
+                Channel = "iiif-img",
+                Policy = null,
+            },
+            new()
+            {
+                Channel = "thumbs",
+                Policy = null,
+            },
+            new()
+            {
+                Channel = "file",
+                Policy = null,
+            }
+        });
+    }
+    
+    
+    [Fact]
+    public async Task DeliveryChannelsConverter_Supports_Complex_Channels()
+    {
+        var hydraAssetBody = @"{
+          ""@type"": ""Image"",
+          ""origin"": ""https://example.org/asset.tiff"",
+          ""family"": ""I"",
+          ""mediaType"": ""image/tiff"",
+            ""deliveryChannels"": [
+                {
+                  ""channel"": ""iiif-img"",
+                  ""policy"": ""default""
+                },
+                {
+                  ""channel"": ""thumbs"",
+                  ""policy"": ""my-thumbs-policy""
+                    
+                },
+                {
+                  ""channel"": ""file"",
+                  ""policy"": ""none""
+                }
+              ],
+        }";
+        
+        var deliveryChannel = JsonConvert.DeserializeObject<Image>(hydraAssetBody);
+        
+        deliveryChannel!.DeliveryChannels!.Length.Should().Be(3);
+        deliveryChannel!.DeliveryChannels!.Should().BeEquivalentTo(new DeliveryChannel[]
+        {
+            new()
+            {
+                Channel = "iiif-img",
+                Policy = "default"
+            },
+            new()
+            {
+                Channel = "thumbs",
+                Policy = "my-thumbs-policy"
+            },
+            new()
+            {
+                Channel = "file",
+                Policy = "none"
+            }
+        });
+    }
+}
---
+++ src/protagonist/DLCS.HydraModel/Converters/ImageDeliveryChannelsConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DLCS.HydraModel.Converters;
+
+public class ImageDeliveryChannelsConverter : JsonConverter
+{
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        serializer.Serialize(writer, value);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        List<DeliveryChannel> deliveryChannels = new();
+        if (reader.TokenType == JsonToken.StartArray)
+        {
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.EndArray)
+                {
+                    break;
+                }
+                if (reader.TokenType == JsonToken.StartObject)
+                {
+                    var newDeliveryChannel = serializer.Deserialize<DeliveryChannel>(reader);
+                    deliveryChannels.Add(newDeliveryChannel);
+                }
+                if (reader.TokenType == JsonToken.String)
+                {
+                    var channel = serializer.Deserialize<string>(reader);
+                    var newDeliveryChannel = new DeliveryChannel()
+                    {
+                        Channel = channel
+                    };
+                    
+                    deliveryChannels.Add(newDeliveryChannel);
+                } 
+            }
+        }
+        
+        return deliveryChannels.ToArray();
+    }
+    
+    public override bool CanConvert(Type objectType)
+    {
+        return true;
+    }
+}
