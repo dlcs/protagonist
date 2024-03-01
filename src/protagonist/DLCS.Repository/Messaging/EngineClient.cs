c77e764a0f28c73513d9690935bf54c3202ee517
jack.lewis <jack.lewis@digirati.com>
2024-03-01 17:41:07 +0000

    removing from preserve from engine client

--- src/protagonist/DLCS.Repository/Messaging/EngineClient.cs
+++ src/protagonist/DLCS.Repository/Messaging/EngineClient.cs
@@ -27,10 +27,7 @@ public class EngineClient : IEngineClient
     private readonly ILogger<EngineClient> logger;
     private readonly DlcsSettings dlcsSettings;
 
-    private static readonly JsonSerializerOptions SerializerOptions = new (JsonSerializerDefaults.Web)
-    {
-        ReferenceHandler = ReferenceHandler.Preserve
-    };
+    private static readonly JsonSerializerOptions SerializerOptions = new (JsonSerializerDefaults.Web);
 
     public EngineClient(
         IQueueLookup queueLookup,
