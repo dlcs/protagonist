e8203637a2e5c2c3a4ab02f7f6f236b9ec60a746
griffri <Griffri.Melville@digirati.com>
2024-03-01 16:50:51 +0000

    Inherit DlcsResource from DeliveryChannel, set nullable context to null

--- src/protagonist/DLCS.HydraModel/DeliveryChannel.cs
+++ src/protagonist/DLCS.HydraModel/DeliveryChannel.cs
@@ -4,14 +4,38 @@ using Newtonsoft.Json;
 
 namespace DLCS.HydraModel;
 
-public class DeliveryChannel
+
+[HydraClass(typeof(DeliveryChannelClass),
+    Description = "A delivery channel represents a way an asset on the DLCS can be served.",
+    UriTemplate = "")]
+public class DeliveryChannel : DlcsResource
 {
-    [JsonProperty(PropertyName = "@type")]
-    public string? Context => "vocab:DeliveryChannel";
+    public override string? Context => null;
     
+    [RdfProperty(Description = "The name of the DLCS delivery channel this is based on.",
+        Range = Names.XmlSchema.String, ReadOnly = false, WriteOnly = false)]
     [JsonProperty(Order = 11, PropertyName = "channel")]
     public string? Channel { get; set; }
     
+    [HydraLink(Description = "The policy assigned to this delivery channel.",
+        Range = "vocab:deliveryChannelPolicy", ReadOnly = false, WriteOnly = false)]
     [JsonProperty(Order = 12, PropertyName = "policy")]
     public string? Policy { get; set; }
+}
+
+public class DeliveryChannelClass: Class
+{
+    string operationId = "_:deliveryChannel_";
+    
+    public DeliveryChannelClass()
+    {
+        BootstrapViaReflection(typeof(DeliveryChannel));
+    }
+    
+    public override void DefineOperations()
+    {
+        SupportedOperations = CommonOperations.GetStandardResourceOperations(
+            operationId, "Delivery Channel", Id,
+            "GET", "POST", "PUT", "PATCH", "DELETE");
+    }
 }
