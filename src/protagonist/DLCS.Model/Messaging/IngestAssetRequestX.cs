3afbf8c659f0742729f9900cd2f8c45254a0bae2
griffri <Griffri.Melville@digirati.com>
2024-03-12 15:23:36 +0000

    Remove redundant comma

--- src/protagonist/DLCS.Model/Messaging/IngestAssetRequestX.cs
+++ src/protagonist/DLCS.Model/Messaging/IngestAssetRequestX.cs
@@ -6,6 +6,6 @@ public static class IngestAssetRequestX
 {
     public static IngestAssetRequest GetMinimalPayload(this IngestAssetRequest ingestAssetRequest)
     {
-        return new IngestAssetRequest(new Asset() { Id = ingestAssetRequest.Asset.Id, }, ingestAssetRequest.Created);
+        return new IngestAssetRequest(new Asset() { Id = ingestAssetRequest.Asset.Id }, ingestAssetRequest.Created);
     }
 }
