14d0e76ecb9ffe1d5215e5ed0b0463b05a34e436
griffri <Griffri.Melville@digirati.com>
2024-03-14 15:15:34 +0000

    Add summary to RetrieveAvChannelPolicyOptions

--- src/protagonist/DLCS.Model/DeliveryChannels/IAvChannelPolicyOptionsRepository.cs
+++ src/protagonist/DLCS.Model/DeliveryChannels/IAvChannelPolicyOptionsRepository.cs
@@ -5,5 +5,8 @@ namespace DLCS.Model.DeliveryChannels;
 
 public interface IAvChannelPolicyOptionsRepository
 {
+    /// <summary>
+    /// Retrieves a list of possible transcode policies for the iiif-av delivery channel
+    /// </summary>
     public Task<IReadOnlyCollection<string>?> RetrieveAvChannelPolicyOptions();
 }
