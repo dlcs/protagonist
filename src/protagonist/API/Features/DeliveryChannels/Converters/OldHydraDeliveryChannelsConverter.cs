756a79e193b9fb287e6d9d423bf5c8fa8ec7e0fc
griffri <Griffri.Melville@digirati.com>
2024-03-28 14:40:48 +0000

    Add comments

--- src/protagonist/API/Features/DeliveryChannels/Converters/OldHydraDeliveryChannelsConverter.cs
+++ src/protagonist/API/Features/DeliveryChannels/Converters/OldHydraDeliveryChannelsConverter.cs
@@ -5,12 +5,18 @@ using DLCS.Model.Assets;
 
 namespace API.Features.DeliveryChannels.Converters;
 
+/// <summary>
+/// Conversion between asset.WcDeliveryChannels and asset.DeliveryChannels
+/// </summary>
 public class OldHydraDeliveryChannelsConverter
 {
     private const string ImageDefaultPolicy = "default";
     private const string ImageUseOriginalPolicy = "use-original";
     private const string FileNonePolicy = "none";
     
+    /// <summary>
+    /// Convert an asset's WcDeliveryChannels into a list of HydraModel.DeliveryChannel objects
+    /// </summary>
     public DeliveryChannel[]? Convert(DLCS.HydraModel.Image hydraImage)
     {
         if (hydraImage.WcDeliveryChannels.IsNullOrEmpty()) return null;
