74b39ab26e4fde8a027728ada250dc86dd0c5684
Donald Gray <donald.gray@digirati.com>
2024-04-11 17:49:46 +0100

    DeliveryChannelPolicyRepository uses RetrieveDeliveryChannel query

--- src/protagonist/API/Features/DeliveryChannels/Helpers/QueryableExtensions.cs
+++ src/protagonist/API/Features/DeliveryChannels/Helpers/QueryableExtensions.cs
@@ -7,7 +7,15 @@ public static class QueryableExtensions
 {
     private const int AdminCustomer = 1;
     
-    public static DeliveryChannelPolicy RetrieveDeliveryChannel(this IQueryable<DeliveryChannelPolicy> policies, int customerId, string channel, string policy)
+    /// <summary>
+    /// Find matching policy, this will be either (in order of precedence):
+    /// Non system policy where channel and name match OR
+    /// System policy where channel and name match.
+    ///
+    /// NOTE: will throw InvalidOperationException if no match found
+    /// </summary>
+    /// <exception cref="InvalidOperationException"> if record not found</exception>
+    public static DeliveryChannelPolicy RetrieveDeliveryChannel(this IEnumerable<DeliveryChannelPolicy> policies, int customerId, string channel, string policy)
     {
         return policies.Single(p =>
             (p.Customer == customerId &&
