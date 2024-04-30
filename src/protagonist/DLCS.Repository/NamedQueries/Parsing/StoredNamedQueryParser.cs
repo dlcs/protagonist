6c251980c969214603fe530eb77fb64c80387506
jack.lewis <jack.lewis@digirati.com>
2024-04-30 16:01:27 +0100

    removing unneeded usings

--- src/protagonist/DLCS.Repository/NamedQueries/Parsing/StoredNamedQueryParser.cs
+++ src/protagonist/DLCS.Repository/NamedQueries/Parsing/StoredNamedQueryParser.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using DLCS.Core.Collections;
 using DLCS.Core.Strings;
 using DLCS.Model.Assets.NamedQueries;
