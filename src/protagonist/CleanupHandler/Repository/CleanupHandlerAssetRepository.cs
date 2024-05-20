633e560704ad097fc030907ea02025ae5075fbc3
jack.lewis <jack.lewis@digirati.com>
2024-05-20 13:55:30 +0100

    test boilerplate for asset modified cleanup

---
+++ src/protagonist/CleanupHandler/Repository/CleanupHandlerAssetRepository.cs
@@ -0,0 +1,21 @@
+﻿using DLCS.Core.Types;
+using DLCS.Model.Assets;
+using DLCS.Repository;
+using DLCS.Repository.Assets;
+
+namespace CleanupHandler.Repository;
+
+public class CleanupHandlerAssetRepository : ICleanupHandlerAssetRepository
+{
+    private readonly DlcsContext dbContext;
+    
+    public CleanupHandlerAssetRepository(DlcsContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    
+    public Asset? RetrieveAssetWithDeliveryChannels(AssetId assetId)
+    {
+        return dbContext.Images.IncludeDeliveryChannelsWithPolicy().SingleOrDefault(x => x.Id == assetId);
+    }
+}
---
+++ src/protagonist/CleanupHandler/Repository/ICleanupHandlerAssetRepository.cs
@@ -0,0 +1,14 @@
+﻿using DLCS.Core.Types;
+using DLCS.Model.Assets;
+
+namespace CleanupHandler.Repository;
+
+public interface ICleanupHandlerAssetRepository
+{
+    /// <summary>
+    /// Retrieves an asset from the database with attached delivery channel policies
+    /// </summary>
+    /// <param name="assetId">The asset id to retrieve details for</param>
+    /// <returns>an asset</returns>
+    Asset? RetrieveAssetWithDeliveryChannels(AssetId assetId);
+}
