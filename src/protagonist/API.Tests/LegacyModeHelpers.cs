3e511e95b516a4b3cfd7a6bf4a975df3ff90a00c
Donald Gray <donald.gray@digirati.com>
2024-06-10 12:49:39 +0100

    Simplify LegacyCustomer test setup
    Introduce helper method to create customer + spaces. Use constants when
    using Legacy and prefer default Customer 99 where possible

---
+++ src/protagonist/API.Tests/LegacyModeHelpers.cs
@@ -0,0 +1,26 @@
+﻿using DLCS.Repository;
+using Test.Helpers.Integration;
+
+namespace API.Tests;
+
+/// <summary>
+/// Helper methods and constants for working with LegacyMode. LegacyCustomer + Space are configured as such via
+/// appsettings.Testing.json
+/// </summary>
+public class LegacyModeHelpers
+{
+    public const int LegacyCustomer = 325665;
+    public const int LegacySpace = 201;
+    public const int NonLegacySpace = 4;
+    
+    public static async Task SetupLegacyCustomer(DlcsContext dbContext, int space = LegacySpace)
+    {
+        await dbContext.Customers.AddTestCustomer(LegacyCustomer);
+        await dbContext.Spaces.AddTestSpace(LegacyCustomer, space);
+        await dbContext.Spaces.AddTestSpace(LegacyCustomer, NonLegacySpace);
+        await dbContext.CustomerStorages.AddTestCustomerStorage(LegacyCustomer);
+        await dbContext.DefaultDeliveryChannels.AddTestDefaultDeliveryChannels(LegacyCustomer);
+        await dbContext.DeliveryChannelPolicies.AddTestDeliveryChannelPolicies(LegacyCustomer);
+        await dbContext.SaveChangesAsync();
+    }
+}
