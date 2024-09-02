d5e7dc8a7e8d49e20a216559beddef96a57719bb
p-kaczynski <p.a.kaczynski@gmail.com>
2024-09-02 13:27:01 +0100

    Add JWT auth parallel to existing basic auth

---
+++ src/protagonist/API/Auth/JwtAuthHelper.cs
@@ -0,0 +1,16 @@
+﻿using DLCS.Core.Settings;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Auth;
+
+public class JwtAuthHelper(IOptions<DlcsSettings> dlcsSettings)
+{
+    public SigningCredentials? SigningCredentials { get; } = dlcsSettings.Value.JwtKey is {Length: > 0} key
+        ? new(
+            new SymmetricSecurityKey(Convert.FromBase64String(key)),
+            SecurityAlgorithms.HmacSha256Signature)
+        : null;
+
+    public string[] ValidIssuers { get; } = dlcsSettings.Value.JwtValidIssuers.ToArray();
+}
