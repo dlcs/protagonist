a3c8c522a0a945556591bc4bd367411caa75e109
jack.lewis <jack.lewis@digirati.com>
2025-04-02 16:45:55 +0100

    Add new manifest column to images table

--- src/protagonist/DLCS.Repository/NamedQueries/Parsing/BaseNamedQueryParser.cs
+++ src/protagonist/DLCS.Repository/NamedQueries/Parsing/BaseNamedQueryParser.cs
@@ -49,11 +49,11 @@ public abstract class BaseNamedQueryParser<T> : INamedQueryParser
         Logger = logger;
     }
 
-    public T2 GenerateParsedNamedQueryFromRequest<T2>(
+    public T GenerateParsedNamedQueryFromRequest<T>(
         int customerId,
         string? namedQueryArgs,
         string namedQueryTemplate,
-        string namedQueryName) where T2 : ParsedNamedQuery
+        string namedQueryName) where T : ParsedNamedQuery
     {
         namedQueryTemplate.ThrowIfNullOrWhiteSpace(nameof(namedQueryTemplate));
 
@@ -67,7 +67,7 @@ public abstract class BaseNamedQueryParser<T> : INamedQueryParser
         var assetQuery = GenerateParsedNamedQuery(customerId, templatePairing, queryArgs);
         assetQuery.NamedQueryName = namedQueryName;
         PostParsingOperations(assetQuery);
-        return (assetQuery as T2)!;
+        return (assetQuery as T)!;
     }
 
     /// <summary>
