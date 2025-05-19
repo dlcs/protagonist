0699d7f28abcbf8abd5098284ab90d224b256883
jack.lewis <jack.lewis@digirati.com>
2025-05-19 16:56:08 +0100

    Match batch names in BatchCompletedNotification

--- src/protagonist/DLCS.AWS/SNS/BatchCompletedNotification.cs
+++ src/protagonist/DLCS.AWS/SNS/BatchCompletedNotification.cs
@@ -2,33 +2,21 @@ using DLCS.Model.Assets;
 
 namespace DLCS.AWS.SNS;
 
-public class BatchCompletedNotification
+public class BatchCompletedNotification(Batch completedBatch)
 {
-    public int Id { get; private set; }
-    
-    public int CustomerId { get; private set; }
-    
-    public int Total { get; private set; }
-    
-    public int Success { get; private set; }
-    
-    public int Errors { get; private set; }
-    
-    public bool Superseded { get; private set; }
-    
-    public DateTime Started { get; private set; }
-    
-    public DateTime? Finished { get; private set; }
+    public int Id { get; private set; } = completedBatch.Id;
 
-    public BatchCompletedNotification(Batch completedBatch)
-    {
-        Id = completedBatch.Id;
-        CustomerId = completedBatch.Customer;
-        Total = completedBatch.Count;
-        Success = completedBatch.Completed;
-        Errors = completedBatch.Errors;
-        Superseded = completedBatch.Superseded;
-        Started = completedBatch.Submitted;
-        Finished = completedBatch.Finished;
-    }
+    public int Customer { get; private set; } = completedBatch.Customer;
+
+    public int Count { get; private set; } = completedBatch.Count;
+
+    public int Completed { get; private set; } = completedBatch.Completed;
+
+    public int Errors { get; private set; } = completedBatch.Errors;
+
+    public bool Superseded { get; private set; } = completedBatch.Superseded;
+
+    public DateTime Submitted { get; private set; } = completedBatch.Submitted;
+
+    public DateTime? Finished { get; private set; } = completedBatch.Finished;
 }
