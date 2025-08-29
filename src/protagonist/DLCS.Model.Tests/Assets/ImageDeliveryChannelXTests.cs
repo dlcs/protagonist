433dbe976fd399b8f793fc8bb69fd793fa00fac8
Donald Gray <donald.gray@digirati.com>
2025-08-29 14:54:35 +0100

    Helper for reading Timebased delivery channel data
    Inline with thumbnails - part of MediaConvert refactor

--- src/protagonist/DLCS.Model.Tests/Assets/ImageDeliveryChannelXTests.cs
+++ src/protagonist/DLCS.Model.Tests/Assets/ImageDeliveryChannelXTests.cs
@@ -75,4 +75,65 @@ public class ImageDeliveryChannelXTests
         var idcs = new List<ImageDeliveryChannel> { new() { Channel = "iiif-img" }, thumbsChannel };
         idcs.GetThumbsChannel(true).Should().Be(thumbsChannel);
     }
+    
+    [Fact]
+    public void GetTimebasedChannel_ThrowIfNotFoundFalse_ReturnsNull_IfListNull()
+    {
+        List<ImageDeliveryChannel> idcs = null;
+        idcs.GetTimebasedChannel(false).Should().BeNull();
+    }
+    
+    [Fact]
+    public void GetTimebasedChannel_ThrowIfNotFoundFalse_ReturnsNull_IfListEmpty()
+    {
+        var idcs = new List<ImageDeliveryChannel>();
+        idcs.GetTimebasedChannel(false).Should().BeNull();
+    }
+    
+    [Fact]
+    public void GetTimebasedChannel_ThrowIfNotFoundFalse_ReturnsNull_IfTimebasedNotFound()
+    {
+        var idcs = new List<ImageDeliveryChannel> { new() { Channel = "iiif-img" } };
+        idcs.GetTimebasedChannel(false).Should().BeNull();
+    }
+    
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void GetTimebasedChannel_ReturnsTimebased_IfFound(bool throwIfNotFound)
+    {
+        var timebasedChannel = new ImageDeliveryChannel
+        {
+            Channel = "iiif-av", DeliveryChannelPolicyId = 12354
+        };
+        var idcs = new List<ImageDeliveryChannel> { new() { Channel = "iiif-img" }, timebasedChannel };
+        idcs.GetTimebasedChannel(throwIfNotFound).Should().Be(timebasedChannel);
+    }
+    
+    [Fact]
+    public void GetTimebasedChannel_ThrowIfNotFoundTrue_Throws_IfListNull()
+    {
+        List<ImageDeliveryChannel> idcs = null;
+        Action action = () => idcs.GetTimebasedChannel(true);
+
+        action.Should().ThrowExactly<InvalidOperationException>();
+    }
+    
+    [Fact]
+    public void GetTimebasedChannel_ThrowIfNotFoundTrue_Throws_IfListEmpty()
+    {
+        var idcs = new List<ImageDeliveryChannel>();
+        Action action = () => idcs.GetTimebasedChannel(true);
+
+        action.Should().ThrowExactly<InvalidOperationException>();
+    }
+    
+    [Fact]
+    public void GetTimebasedChannel_ThrowIfNotFoundTrue_Throws_IfTimebasedNotFound()
+    {
+        var idcs = new List<ImageDeliveryChannel> { new() { Channel = "iiif-img" } };
+        Action action = () => idcs.GetTimebasedChannel(true);
+
+        action.Should().ThrowExactly<InvalidOperationException>();
+    }
 }
