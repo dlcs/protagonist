245fdc531c48a24ca3a2d178d22d0a7029a7798e
Donald Gray <donald.gray@digirati.com>
2025-10-01 17:45:06 +0100

    Allow http origins larger than 2GB
    Updated behaviour of BasicHttp + Default strategy to handle files
    larger than 2GB

--- src/protagonist/DLCS.Repository/Strategy/DefaultOriginStrategy.cs
+++ src/protagonist/DLCS.Repository/Strategy/DefaultOriginStrategy.cs
@@ -11,30 +11,20 @@ namespace DLCS.Repository.Strategy;
 /// <summary>
 /// OriginStrategy implementation for 'default' assets.
 /// </summary>
-public class DefaultOriginStrategy : IOriginStrategy
+public class DefaultOriginStrategy(IHttpClientFactory httpClientFactory, ILogger<DefaultOriginStrategy> logger)
+    : IOriginStrategy
 {
-    private readonly IHttpClientFactory httpClientFactory;
-    private readonly ILogger<DefaultOriginStrategy> logger;
-    
-    public DefaultOriginStrategy(IHttpClientFactory httpClientFactory, ILogger<DefaultOriginStrategy> logger)
-    {
-        this.httpClientFactory = httpClientFactory;
-        this.logger = logger;
-    }
-
     public async Task<OriginResponse> LoadAssetFromOrigin(AssetId assetId, string origin,
         CustomerOriginStrategy? customerOriginStrategy, CancellationToken cancellationToken = default)
     {
-        // NOTE(DG): This will follow up to 8 redirections, as per deliverator.
-        // However, https -> http will fail. 
-        // Need to test relative redirects too.
         logger.LogDebug("Fetching {Asset} from Origin: {Url}", assetId, origin);
 
         try
         {
             var httpClient = httpClientFactory.CreateClient(HttpClients.OriginStrategy);
-            var response = await httpClient.GetAsync(origin, cancellationToken);
-            var originResponse = await CreateOriginResponse(response);
+            var response =
+                await httpClient.GetAsync(origin, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+            var originResponse = await response.CreateOriginResponse(cancellationToken);
             return originResponse;
         }
         catch (Exception ex)
@@ -42,19 +32,5 @@ public class DefaultOriginStrategy : IOriginStrategy
             logger.LogError(ex, "Error fetching {Asset} from Origin: {Url}", assetId, origin);
             return OriginResponse.Empty;
         }
-    }
-    
-    private static async Task<OriginResponse> CreateOriginResponse(HttpResponseMessage response)
-    {
-        response.EnsureSuccessStatusCode();
-        var content = response.Content;
-        if (content == null)
-        {
-            return OriginResponse.Empty;
-        }
-
-        return new OriginResponse(await content.ReadAsStreamAsync())
-            .WithContentLength(content.Headers.ContentLength)
-            .WithContentType(content.Headers?.ContentType?.MediaType);
     }
 }
