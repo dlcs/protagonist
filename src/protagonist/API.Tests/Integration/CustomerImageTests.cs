546b0ca714c8119d0b85cc2d0c0c7f80dffda88e
jack.lewis <jack.lewis@digirati.com>
2025-10-09 16:25:47 +0100

    Add test to show alphabetical reordering

--- src/protagonist/API.Tests/Integration/CustomerImageTests.cs
+++ src/protagonist/API.Tests/Integration/CustomerImageTests.cs
@@ -213,6 +213,7 @@ public class CustomerImageTests : IClassFixture<ProtagonistAppFactory<Startup>>
     [InlineData("first,second", "[]", "remove", "first", "second")]
     [InlineData("first,second", "[\"first\"]", "add", "first", "second")]
     [InlineData("first", "[\"first\",\"second\"]", "add", "first", "second")]
+    [InlineData("b", "[\"a\",\"c\"]", "add", "a", "b", "c")]
     public async Task Patch_AllImages_TestManifestPermutations(string initial, string update, string operation, params string[] result)
     {
         // Arrange
