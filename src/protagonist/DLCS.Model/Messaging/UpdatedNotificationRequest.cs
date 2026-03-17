b980b7c424e9284afcc22615e3993e35f82c45b2
jack.lewis <jack.lewis@digirati.com>
2026-03-17 17:11:13 +0000

    remove todo

--- src/protagonist/DLCS.Model/Messaging/UpdatedNotificationRequest.cs
+++ src/protagonist/DLCS.Model/Messaging/UpdatedNotificationRequest.cs
@@ -3,7 +3,7 @@ using DLCS.Model.PathElements;
 
 namespace DLCS.Model.Messaging;
 
-public class UpdatedNotificationRequest<T> where T : IDeliverable //todo: name changes need to be called out on PR
+public class UpdatedNotificationRequest<T> where T : IDeliverable
 {
     public T? DeliverableBeforeUpdate { get; set; }
     
