0b12eb4d03c08464d92e964c6440dbf3a1eebec9
p-kaczynski <p.a.kaczynski@gmail.com>
2026-03-31 09:30:47 +0100

    Initial work on multiple adjunct posting

---
+++ src/protagonist/DLCS.HydraModel/ItemArrayOrHydraCollection.cs
@@ -0,0 +1,18 @@
+﻿using DLCS.HydraModel.Converters;
+using Hydra;
+using Hydra.Collections;
+using Newtonsoft.Json;
+
+namespace DLCS.HydraModel;
+
+/// <summary>
+/// Used as a wrapper/guide for deserializer to accept either <typeparamref name="T"/>, or plain JSON array of
+/// <typeparamref name="T"/>, or a <see cref="HydraCollection{T}"/>
+/// </summary>
+/// <param name="items">Used by deserializing converter to set item or items extracted from JSON payload</param>
+/// <typeparam name="T">Any valid <see cref="JsonLdBase"/> derrivative</typeparam>
+public class ItemArrayOrHydraCollection<T>(params T[] items)
+    where T : JsonLdBase
+{
+    public T[] Items { get; } = items;
+}
