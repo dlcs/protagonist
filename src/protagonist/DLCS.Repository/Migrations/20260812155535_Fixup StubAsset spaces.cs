8f9ee3a5f17782aa391b260e035e8913512bbb82
Donald Gray <donald.gray@digirati.com>
2026-08-12 17:00:34 +0100

    Fixup issue with stub-asset tags + roles values

---
+++ src/protagonist/DLCS.Repository/Migrations/20260812155535_Fixup StubAsset spaces.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DLCS.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixupStubAssetspaces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // SpaceZeroHandling set "Tags" and "Roles" to ARRAY[]::text[], which results in '{}'. This resets
+            // those to and empty string.
+            migrationBuilder.Sql("UPDATE \"Spaces\" SET \"Tags\" = '' WHERE \"Tags\" = '{}' and \"Id\" = 0;");
+            migrationBuilder.Sql("UPDATE \"Spaces\" SET \"Roles\" = '' WHERE \"Roles\" = '{}' and \"Id\" = 0;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // No down script, this was in error so no need to set it back.
+        }
+    }
+}
